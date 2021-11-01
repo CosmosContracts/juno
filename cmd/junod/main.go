67093084376b4fa605665e8e687a9da67edaac28
Joe Abbey <joe.abbey@gmail.com>
2021-11-01 15:06:49 +0000

    Adding rollback to Juno CLI to workaround AppHash mismatches

--- cmd/junod/main.go
+++ cmd/junod/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/CosmosContracts/juno/app"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+	tmcmds "github.com/tendermint/tendermint/cmd/tendermint/commands"
 	"github.com/tendermint/spm/cosmoscmd"
 )
 
@@ -20,7 +21,7 @@ func main() {
 		// this line is used by starport scaffolding # root/arguments
 		cmdOptions...,
 	)
-
+	rootCmd.AddCommand(tmcmds.RollbackStateCmd)
 	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
 		os.Exit(1)
 	}
