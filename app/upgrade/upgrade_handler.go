f6dd74ec732315187a5cf374133a268899011b5a
romelukaku <gundamaster5@gmail.com>
2022-03-14 05:05:30 +0700

    refactor logic again

--- app/upgrade/upgrade_handler.go
+++ app/upgrade/upgrade_handler.go
@@ -15,52 +15,39 @@ var addressesToBeAdjusted = []string{
 	"juno1aeh8gqu9wr4u8ev6edlgfq03rcy6v5twfn0ja8",
 }
 
-func MoveDelegatorDelegationsToCommunityPool(ctx sdk.Context, delAcc sdk.AccAddress, staking *stakingkeeper.Keeper, bank *bankkeeper.BaseKeeper, distr *distrkeeper.Keeper) {
+func MoveAccountCoinToCommunityPool(ctx sdk.Context, accAddr sdk.AccAddress, staking *stakingkeeper.Keeper, bank *bankkeeper.BaseKeeper, distr *distrkeeper.Keeper) {
 	bondDenom := staking.BondDenom(ctx)
-	delegatorDelegations := staking.GetAllDelegatorDelegations(ctx, delAcc)
 
-	amountToBeMovedFromNotBondedPool := sdk.ZeroInt()
-	amountToBeMovedFromBondedPool := sdk.ZeroInt()
-
-	for _, delegation := range delegatorDelegations {
-
+	// this loop will turn all delegator's delegations into unbonding delegations
+	for _, delegation := range staking.GetAllDelegatorDelegations(ctx, accAddr) {
 		validatorValAddr := delegation.GetValidatorAddr()
-		validator, found := staking.GetValidator(ctx, validatorValAddr)
+		_, found := staking.GetValidator(ctx, validatorValAddr)
 		if !found {
 			continue
 		}
+		staking.Undelegate(ctx, accAddr, validatorValAddr, delegation.GetShares()) //nolint:errcheck // nolint because otherwise we'd have a time and nothing to do with it.
+	}
 
-		unbondedAmount, err := staking.Unbond(ctx, delAcc, validatorValAddr, delegation.GetShares()) //nolint:errcheck // nolint because otherwise we'd have a time and nothing to do with it.
-		if err != nil {
-			panic(err)
+	now := ctx.BlockHeader().Time
+
+	// this loop will complete all delegator's unbonding delegations
+	for _, unbondingDelegation := range staking.GetAllUnbondingDelegations(ctx, accAddr) {
+		// validator address of this unbonding delegation
+		validatorStringAddr := unbondingDelegation.ValidatorAddress
+		validatorValAddr, _ := sdk.ValAddressFromBech32(validatorStringAddr)
+
+		// set all entry completionTime to now so we can complete unbonding delegation
+		for i := range unbondingDelegation.Entries {
+			unbondingDelegation.Entries[i].CompletionTime = now
 		}
-
-		if validator.IsBonded() {
-			amountToBeMovedFromBondedPool = amountToBeMovedFromBondedPool.Add(unbondedAmount)
-		} else {
-			amountToBeMovedFromNotBondedPool = amountToBeMovedFromNotBondedPool.Add(unbondedAmount)
-		}
+		staking.SetUnbondingDelegation(ctx, unbondingDelegation)
+		staking.CompleteUnbonding(ctx, accAddr, validatorValAddr)
 	}
 
-	delegatorUnbondingDelegations := staking.GetAllUnbondingDelegations(ctx, delAcc)
-	for _, unbondingDelegation := range delegatorUnbondingDelegations {
-		for _, entry := range unbondingDelegation.Entries {
-			amountToBeMovedFromNotBondedPool = amountToBeMovedFromNotBondedPool.Add(entry.Balance)
-		}
-		staking.RemoveUnbondingDelegation(ctx, unbondingDelegation)
-	}
-
-	coinsToBeMovedFromNotBondedPool := sdk.NewCoins(sdk.NewCoin(bondDenom, amountToBeMovedFromNotBondedPool))
-	coinsToBeMovedFromBondedPool := sdk.NewCoins(sdk.NewCoin(bondDenom, amountToBeMovedFromBondedPool))
-
-	if !coinsToBeMovedFromNotBondedPool.Empty() {
-		notBondedPoolAcc := staking.GetNotBondedPool(ctx)
-		distr.FundCommunityPool(ctx, coinsToBeMovedFromNotBondedPool, notBondedPoolAcc.GetAddress())
-	}
-	if !coinsToBeMovedFromBondedPool.Empty() {
-		bondedPoolAcc := staking.GetBondedPool(ctx)
-		distr.FundCommunityPool(ctx, coinsToBeMovedFromBondedPool, bondedPoolAcc.GetAddress())
-	}
+	// account balance after finishing unbonding
+	accCoin := bank.GetBalance(ctx, accAddr, bondDenom)
+	// move all coin from that acc to community pool
+	distr.FundCommunityPool(ctx, sdk.NewCoins(accCoin), accAddr)
 }
 
 //CreateUpgradeHandler make upgrade handler
@@ -68,13 +55,10 @@ func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator, 
 	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		for _, addrString := range addressesToBeAdjusted {
 			accAddr, _ := sdk.AccAddressFromBech32(addrString)
-			// unbond the accAddr delegations, send all the unbonding and unbonded tokens to the community pool
-			MoveDelegatorDelegationsToCommunityPool(ctx, accAddr, staking, bank, distr)
-			// send 50k juno from the community pool to the accAddr if the master account has less than 50k juno
-			accAddrAmount := bank.GetBalance(ctx, accAddr, staking.BondDenom(ctx)).Amount
-			if sdk.NewIntFromUint64(50000000000).GT(accAddrAmount) {
-				bank.SendCoinsFromModuleToAccount(ctx, distrtypes.ModuleName, accAddr, sdk.NewCoins(sdk.NewCoin(staking.BondDenom(ctx), sdk.NewIntFromUint64(50000000000).Sub(accAddrAmount))))
-			}
+			// move all juno from acc to community pool (uncluding bonded juno)
+			MoveAccountCoinToCommunityPool(ctx, accAddr, staking, bank, distr)
+			// send 50k juno from the community pool to the accAddr
+			bank.SendCoinsFromModuleToAccount(ctx, distrtypes.ModuleName, accAddr, sdk.NewCoins(sdk.NewCoin(staking.BondDenom(ctx), sdk.NewIntFromUint64(50000000000))))
 		}
 		// force an update of validator min commission
 		// we already did this for moneta
