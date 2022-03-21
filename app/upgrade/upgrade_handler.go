5607476c259a490582bb49b23abf8741b8d1b5d6
Jacob Gadikian <jacobgadikian@gmail.com>
2022-03-21 11:53:54 +0700

    good lupercalia upgrade

---
+++ app/upgrade/upgrade_handler.go
@@ -0,0 +1,56 @@
+package lupercalia
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
+)
+
+//CreateUpgradeHandler make upgrade handler
+func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator, staking *stakingkeeper.Keeper, bank *bankkeeper.BaseKeeper) upgradetypes.UpgradeHandler {
+
+		// force an update of validator min commission
+		// we already did this for moneta
+		// but validators could have snuck in changes in the
+		// interim
+		// and via state sync to post-moneta
+		validators := staking.GetAllValidators(ctx)
+		// hard code this because we don't want
+		// a) a fork or
+		// b) immediate reaction with additional gov props
+		minCommissionRate := sdk.NewDecWithPrec(5, 2)
+		for _, v := range validators {
+			if v.Commission.Rate.LT(minCommissionRate) {
+				if v.Commission.MaxRate.LT(minCommissionRate) {
+					v.Commission.MaxRate = minCommissionRate
+				}
+
+				v.Commission.Rate = minCommissionRate
+				v.Commission.UpdateTime = ctx.BlockHeader().Time
+
+				// call the before-modification hook since we're about to update the commission
+				staking.BeforeValidatorModified(ctx, v.GetOperator())
+
+				staking.SetValidator(ctx, v)
+			}
+		}
+
+		// Set wasm old version to 1 if we want to call wasm's InitGenesis ourselves
+		// in this upgrade logic ourselves
+		// vm[wasm.ModuleName] = wasm.ConsensusVersion
+
+		// otherwise we run this, which will run wasm.InitGenesis(wasm.DefaultGenesis())
+		// and then override it after
+		newVM, err := mm.RunMigrations(ctx, configurator, vm)
+		if err != nil {
+			return newVM, err
+		}
+
+		// override here
+		return newVM, err
+	}
+
+}
