07c1ef99b8f512f112e80ea469979a3568060b84
Alex Lynham <alex@lynh.am>
2022-04-18 20:59:51 +0100

    Bump to our PR onto wasmd

--- app/app.go
+++ app/app.go
@@ -779,6 +779,10 @@ func (app *App) RegisterUpgradeHandlers(cfg module.Configurator) {
 		}
 		return app.mm.RunMigrations(ctx, cfg, vm)
 	})
+
+	app.UpgradeKeeper.SetUpgradeHandler("wasmd-patch", func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		return app.mm.RunMigrations(ctx, cfg, vm)
+	})
 }
 
 // GetMaccPerms returns a copy of the module account permissions
--- go.mod
+++ go.mod
@@ -123,6 +123,7 @@ require (
 )
 
 replace (
+	github.com/CosmWasm/wasmd => github.com/envoylabs/wasmd v0.26.0-alpha
 	github.com/cosmos/iavl => github.com/cosmos/iavl v0.17.3
 	github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
 	google.golang.org/grpc => google.golang.org/grpc v1.33.2
