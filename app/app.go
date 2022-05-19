7220da8b081f6c070e081ef86759a7f0c81351b8
Giancarlos Salas <giansalex@gmail.com>
2022-05-19 11:08:37 -0500

    Fix nil snapshot manager in tests

--- app/app.go
+++ app/app.go
@@ -603,11 +603,13 @@ func New(
 	app.SetAnteHandler(anteHandler)
 	app.SetEndBlocker(app.EndBlocker)
 
-	err = app.SnapshotManager().RegisterExtensions(
-		wasmkeeper.NewWasmSnapshotter(app.CommitMultiStore(), &app.wasmKeeper),
-	)
-	if err != nil {
-		panic("failed to register snapshot extension: " + err.Error())
+	if manager := app.SnapshotManager(); manager != nil {
+		err = manager.RegisterExtensions(
+			wasmkeeper.NewWasmSnapshotter(app.CommitMultiStore(), &app.wasmKeeper),
+		)
+		if err != nil {
+			panic("failed to register snapshot extension: " + err.Error())
+		}
 	}
 
 	if loadLatest {
