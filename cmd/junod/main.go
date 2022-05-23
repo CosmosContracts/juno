d228f5dfe234853bbef3f016dc8d8ea6acd95451
Giancarlos Salas <giansalex@gmail.com>
2022-05-23 04:35:00 -0500

    Revert "Merge pull request #80 from joeabbey/add-rollback-to-cli" (#212)
    This reverts commit e07087055b903aa721596f9e4928780ab8e34a7f, reversing
    changes made to aa76b97e1f481580707b41351beb1e82449669fb.

--- cmd/junod/main.go
+++ cmd/junod/main.go
@@ -6,12 +6,10 @@ import (
 	"github.com/CosmosContracts/juno/app"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/tendermint/starport/starport/pkg/cosmoscmd"
-	tmcmds "github.com/tendermint/tendermint/cmd/tendermint/commands"
 )
 
 func main() {
 	cmdOptions := GetWasmCmdOptions()
-	cmdOptions = append(cmdOptions, cosmoscmd.AddSubCmd(tmcmds.RollbackStateCmd))
 	rootCmd, _ := cosmoscmd.NewRootCmd(
 		app.Name,
 		app.AccountAddressPrefix,
@@ -22,6 +20,7 @@ func main() {
 		// this line is used by starport scaffolding # root/arguments
 		cmdOptions...,
 	)
+
 	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
 		os.Exit(1)
 	}
