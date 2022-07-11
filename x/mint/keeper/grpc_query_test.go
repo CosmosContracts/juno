478950a22df70fad6cc407a9c30ce4a9439fc9d9
Jacob Gadikian <jacobgadikian@gmail.com>
2022-07-11 18:08:49 +0700

    fix grpc query test

--- x/mint/keeper/grpc_query_test.go
+++ x/mint/keeper/grpc_query_test.go
@@ -7,22 +7,22 @@ import (
 	"github.com/stretchr/testify/suite"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
-	"github.com/CosmosContracts/juno/v8/app"
-	"github.com/CosmosContracts/juno/v8/x/mint/types"
 	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/mint/types"
 )
 
 type MintTestSuite struct {
 	suite.Suite
 
-	app         *app.App
+	app         *simapp.SimApp
 	ctx         sdk.Context
 	queryClient types.QueryClient
 }
 
 func (suite *MintTestSuite) SetupTest() {
-	app := setup(false)
+	app := simapp.Setup(suite.T(), false)
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
 
 	queryHelper := baseapp.NewQueryServerTestHelper(ctx, app.InterfaceRegistry())
