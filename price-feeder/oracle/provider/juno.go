fdbb729a1013ba4e469ca129a37a1758a465e601
ThanhNhann <thanhnhan98qh@gmail.com>
2022-11-05 20:07:18 +0700

    done GetTickerPrices function of Juno (dont have tests)

--- price-feeder/oracle/provider/juno.go
+++ price-feeder/oracle/provider/juno.go
@@ -3,20 +3,23 @@ package provider
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	_ "io"
 	"io/ioutil"
 	"net/http"
+	"reflect"
 	"strings"
 
 	"github.com/CosmosContracts/juno/price-feeder/oracle/types"
-	_ "github.com/CosmosContracts/juno/price-feeder/oracle/util"
+	"github.com/CosmosContracts/juno/price-feeder/oracle/util"
 )
 
 const (
-	junoRestURL        = "https://api-junoswap.enigma-validator.com"
-	junoTokenEndpoint  = "/prices/tokens"
-	junoCandleEndpoint = "/prices/tokens/historical"
-	junoPairsEndpoint  = "/summary/pairs"
+	junoRestURL             = "https://api-junoswap.enigma-validator.com"
+	junoPriceTokenEndpoint  = "/prices/tokens"
+	junoVolumeTokenEndpoint = "/volumes/tokens"
+	junoCandleEndpoint      = "/prices/tokens/historical"
+	junoPairsEndpoint       = "/summary/pairs"
 )
 
 var _ Provider = (*JunoProvider)(nil)
@@ -31,10 +34,24 @@ type (
 		client  *http.Client
 	}
 
-	// JunoPairsSummary defines the response structure for an Juno pairs
-	// summary.
-	JunoPairsSummary struct {
-		Data []JunoPairData
+	// JunoTokenPriceResponse defines the response structure of price for an Juno token
+	// request.
+	JunoTokenPriceResponse struct {
+		Price float64 `json:"price"`
+	}
+
+	// JunoTokenVolumnResponse defines the response structure of volume for an Juno token
+	// request.
+	JunoTokenVolumnResponse struct {
+		Volumne float64 `json:"volumes"`
+	}
+
+	// JunoTokenInfo defines the response structure of information of an Juno token
+	// request.
+	JunoTokenInfo struct {
+		JunoTokenPriceResponse
+		Symbol string
+		Volume float64
 	}
 
 	// JunoPairData defines the data response structure for an Juno pair.
@@ -58,7 +75,107 @@ func NewJunoProvider(endpoint Endpoint) *JunoProvider {
 }
 
 func (p JunoProvider) GetTickerPrices(pairs ...types.CurrencyPair) (map[string]types.TickerPrice, error) {
-	return nil, nil
+
+	var junoTokenInfo []JunoTokenInfo
+	// Get price and symbol of tokens
+	pathPriceToken := fmt.Sprintf("%s%s/current", p.baseURL, junoPriceTokenEndpoint)
+	resp, err := p.client.Get(pathPriceToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make Juno request: %w", err)
+	}
+	err = checkHTTPStatus(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	bz, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read Juno response body: %w", err)
+	}
+	var tokensResps map[string]interface{}
+	if err := json.Unmarshal(bz, &tokensResps); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal Juno response body: %w", err)
+	}
+
+	listSymbol := reflect.ValueOf(tokensResps).MapKeys()
+
+	// Get symbol and price of Tokens
+	for _, symbol := range listSymbol {
+		tokenInfo, err := getSymbolAndPriceToken(symbol, tokensResps)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal Juno response body: %w", err)
+		}
+		junoTokenInfo = append(junoTokenInfo, tokenInfo)
+	}
+
+	// Get volume of tokens
+
+	for id, symbol := range listSymbol {
+		path := fmt.Sprintf("%s%s/%s/current", p.baseURL, junoVolumeTokenEndpoint, symbol)
+		resp, err = p.client.Get(path)
+		if err != nil {
+			return nil, fmt.Errorf("failed to make Juno request: %w", err)
+		}
+		err = checkHTTPStatus(resp)
+		if err != nil {
+			return nil, err
+		}
+
+		defer resp.Body.Close()
+
+		bz, err = io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read Juno response body: %w", err)
+		}
+		var tokensResp JunoTokenVolumnResponse
+		if err := json.Unmarshal(bz, &tokensResp); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal Juno response body: %w", err)
+		}
+		println("check datas volume: ", tokensResp.Volumne)
+		junoTokenInfo[id].Volume = tokensResp.Volumne
+	}
+
+	baseDenomIdx := make(map[string]types.CurrencyPair)
+	for _, cp := range pairs {
+		baseDenomIdx[strings.ToUpper(cp.Base)] = cp
+	}
+
+	tickerPrices := make(map[string]types.TickerPrice, len(pairs))
+	for _, tr := range junoTokenInfo {
+		symbol := strings.ToUpper(tr.Symbol) // symbol == base in a currency pair
+
+		cp, ok := baseDenomIdx[symbol]
+		if !ok {
+			// skip tokens that are not requested
+			continue
+		}
+
+		if _, ok := tickerPrices[symbol]; ok {
+			return nil, fmt.Errorf("duplicate token found in Osmosis response: %s", symbol)
+		}
+
+		price, err := util.NewDecFromFloat(tr.Price)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read Osmosis price (%f) for %s", tr.Price, symbol)
+		}
+
+		volume, err := util.NewDecFromFloat(tr.Volume)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read Osmosis volume (%f) for %s", tr.Volume, symbol)
+		}
+		tickerPrices[cp.String()] = types.TickerPrice{Price: price, Volume: volume}
+	}
+
+	for _, cp := range pairs {
+		if _, ok := tickerPrices[cp.String()]; !ok {
+			return nil, fmt.Errorf(types.ErrMissingExchangeRate.Error(), cp.String())
+		}
+	}
+
+	return tickerPrices, nil
+
 }
 
 func (p JunoProvider) GetCandlePrices(pairs ...types.CurrencyPair) (map[string][]types.CandlePrice, error) {
@@ -100,4 +217,19 @@ func (p JunoProvider) GetAvailablePairs() (map[string]struct{}, error) {
 // SubscribeCurrencyPairs performs a no-op since osmosis does not use websockets
 func (p JunoProvider) SubscribeCurrencyPairs(pairs ...types.CurrencyPair) error {
 	return nil
+}
+
+// Get symbol and price of token
+func getSymbolAndPriceToken(symbol reflect.Value, tokensResps map[string]interface{}) (tokenInfo JunoTokenInfo, err error) {
+
+	var tokenPrice JunoTokenPriceResponse
+	tokenInfo.Symbol = symbol.String()
+	dataOfToken := tokensResps[tokenInfo.Symbol]
+	body, _ := json.Marshal(dataOfToken)
+	if err := json.Unmarshal(body, &tokenPrice); err != nil {
+		return tokenInfo, fmt.Errorf("failed to unmarshal Juno response body: %w", err)
+	}
+	tokenInfo.Price = tokenPrice.Price
+
+	return tokenInfo, nil
 }
--- price-feeder/oracle/provider/juno_test.go
+++ price-feeder/oracle/provider/juno_test.go
@@ -5,8 +5,47 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/CosmosContracts/juno/price-feeder/oracle/types"
 	"github.com/stretchr/testify/require"
 )
+
+func TestJunoProvider_GetTickerPrices(t *testing.T) {
+	p := NewJunoProvider(Endpoint{})
+
+	t.Run("valid_request_single_ticker", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			require.Equal(t, "/prices/tokens/current", req.URL.String())
+			resp := `{
+				"JUNO": {
+				  "date": "2022-11-02 18:57:36",
+				  "price": 2.93,
+				  "denom": "ujuno"
+				},
+				"AKT": {
+				  "date": "2022-11-02 18:57:36",
+				  "price": 0.26169002,
+				  "denom": "ibc/DFC6F33796D5D0075C5FB54A4D7B8E76915ACF434CB1EE2A1BA0BB8334E17C3A"
+				},
+				"ARTO": {
+				  "date": "2022-11-02 18:57:36",
+				  "price": 0.08182318000000001,
+				  "denom": "arto"
+				}
+			}
+			`
+			rw.Write([]byte(resp))
+		}))
+		defer server.Close()
+
+		p.client = server.Client()
+		p.baseURL = server.URL
+		//TODO: config JunoProvider to 2 group baseURL and client
+		// one for get price, one for get volume
+		_, err := p.GetTickerPrices(types.CurrencyPair{Base: "ATOM", Quote: "USDT"})
+		// require.NoError(t, err)
+		require.Error(t, err)
+	})
+}
 
 func TestJunoProvider_GetAvailablePairs(t *testing.T) {
 	p := NewJunoProvider(Endpoint{})
