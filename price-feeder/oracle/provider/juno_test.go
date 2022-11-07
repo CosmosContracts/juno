f994ca5d2fc7741f320f13f15da4126731237c37
ThanhNhann <thanhnhan98qh@gmail.com>
2022-11-07 14:35:18 +0700

    update more test case for GetTickerPrices func of Juno

--- price-feeder/oracle/provider/juno_test.go
+++ price-feeder/oracle/provider/juno_test.go
@@ -72,6 +72,143 @@ func TestJunoProvider_GetTickerPrices(t *testing.T) {
 		require.Equal(t, sdk.MustNewDecFromStr("2.93"), prices["JUNOUSDT"].Price)
 		require.Equal(t, sdk.MustNewDecFromStr("5"), prices["JUNOUSDT"].Volume)
 	})
+
+	t.Run("valid_request_multi_ticker", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			//require.Equal(t, "/prices/tokens/current", req.URL.String())
+			println("check req: ", req.URL.String())
+			var resp string
+			if req.URL.String() == "/prices/tokens/current" {
+				resp = `{
+					"JUNO": {
+					"date": "2022-11-02 18:57:36",
+					"price": 2.93,
+					"denom": "ujuno"
+					},
+					"AKT": {
+					"date": "2022-11-02 18:57:36",
+					"price": 0.26169002,
+					"denom": "ibc/DFC6F33796D5D0075C5FB54A4D7B8E76915ACF434CB1EE2A1BA0BB8334E17C3A"
+					},
+					"ARTO": {
+					"date": "2022-11-02 18:57:36",
+					"price": 0.08182318000000001,
+					"denom": "arto"
+					}
+				}
+			`
+			} else if req.URL.String() == "/volumes/tokens/JUNO/current" {
+				resp = `
+				{
+					"date": "2022-11-07",
+					"volumes": 5
+				}
+				`
+			} else if req.URL.String() == "/volumes/tokens/AKT/current" {
+				resp = `
+				{
+					"date": "2022-11-07",
+					"volumes": 5
+				}
+				`
+			} else if req.URL.String() == "/volumes/tokens/ARTO/current" {
+				resp = `
+				{
+					"date": "2022-11-07",
+					"volumes": 0
+				}
+				`
+			}
+			rw.Write([]byte(resp))
+		}))
+		defer server.Close()
+
+		p.client = server.Client()
+		p.baseURL = server.URL
+
+		prices, err := p.GetTickerPrices(
+			types.CurrencyPair{Base: "JUNO", Quote: "USDT"},
+			types.CurrencyPair{Base: "AKT", Quote: "USDT"},
+		)
+		require.NoError(t, err)
+		require.Len(t, prices, 2)
+		require.Equal(t, sdk.MustNewDecFromStr("2.93"), prices["JUNOUSDT"].Price)
+		require.Equal(t, sdk.MustNewDecFromStr("5"), prices["JUNOUSDT"].Volume)
+		require.Equal(t, sdk.MustNewDecFromStr("0.26169002"), prices["AKTUSDT"].Price)
+		require.Equal(t, sdk.MustNewDecFromStr("5"), prices["AKTUSDT"].Volume)
+	})
+
+	t.Run("invalid_request_bad_response", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			rw.Write([]byte(`FOO`))
+		}))
+		defer server.Close()
+
+		p.client = server.Client()
+		p.baseURL = server.URL
+
+		prices, err := p.GetTickerPrices(types.CurrencyPair{Base: "JUNO", Quote: "USDT"})
+		require.Error(t, err)
+		require.Nil(t, prices)
+	})
+
+	t.Run("invalid_request_invalid_ticker", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			//require.Equal(t, "/prices/tokens/current", req.URL.String())
+			println("check req: ", req.URL.String())
+			var resp string
+			if req.URL.String() == "/prices/tokens/current" {
+				resp = `{
+					"JUNO": {
+					"date": "2022-11-02 18:57:36",
+					"price": 2.93,
+					"denom": "ujuno"
+					},
+					"AKT": {
+					"date": "2022-11-02 18:57:36",
+					"price": 0.26169002,
+					"denom": "ibc/DFC6F33796D5D0075C5FB54A4D7B8E76915ACF434CB1EE2A1BA0BB8334E17C3A"
+					},
+					"ARTO": {
+					"date": "2022-11-02 18:57:36",
+					"price": 0.08182318000000001,
+					"denom": "arto"
+					}
+				}
+			`
+			} else if req.URL.String() == "/volumes/tokens/JUNO/current" {
+				resp = `
+				{
+					"date": "2022-11-07",
+					"volumes": 5
+				}
+				`
+			} else if req.URL.String() == "/volumes/tokens/AKT/current" {
+				resp = `
+				{
+					"date": "2022-11-07",
+					"volumes": 0
+				}
+				`
+			} else if req.URL.String() == "/volumes/tokens/ARTO/current" {
+				resp = `
+				{
+					"date": "2022-11-07",
+					"volumes": 0
+				}
+				`
+			}
+			rw.Write([]byte(resp))
+		}))
+		defer server.Close()
+
+		p.client = server.Client()
+		p.baseURL = server.URL
+
+		prices, err := p.GetTickerPrices(types.CurrencyPair{Base: "FOO", Quote: "BAR"})
+		require.Error(t, err)
+		require.Nil(t, prices)
+	})
 }
 
 func TestJunoProvider_GetAvailablePairs(t *testing.T) {
