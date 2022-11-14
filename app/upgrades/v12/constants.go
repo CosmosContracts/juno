1d42f02894dcb25910d096605219ba2fafd3c09a
Reece Williams <reecepbcups@gmail.com>
2022-11-14 13:15:38 -0600

    upgrade handler for token factory

---
+++ app/upgrades/v12/constants.go
@@ -0,0 +1,18 @@
+package v11
+
+import (
+	tokenfactorytypes "github.com/CosmWasm/token-factory/x/tokenfactory/types"
+	"github.com/CosmosContracts/juno/v12/app/upgrades"
+	store "github.com/cosmos/cosmos-sdk/store/types"
+)
+
+// UpgradeName defines the on-chain upgrade name for the Juno v11 upgrade.
+const UpgradeName = "v12"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateV12UpgradeHandler,
+	StoreUpgrades: store.StoreUpgrades{
+		Added: []string{tokenfactorytypes.ModuleName},
+	},
+}
---
+++ app/upgrades/v12/upgrades.go
@@ -0,0 +1,19 @@
+package v11
+
+import (
+	"github.com/CosmosContracts/juno/v12/app/keepers"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+// CreateV12UpgradeHandler makes an upgrade handler for v12 of Juno
+func CreateV12UpgradeHandler(
+	mm *module.Manager,
+	cfg module.Configurator,
+	keepers *keepers.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		return mm.RunMigrations(ctx, cfg, vm)
+	}
+}
