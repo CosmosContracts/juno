c7644a72e872314f48f7a272579b7dc7799db44e
Reece Williams <31943163+Reecepbcups@users.noreply.github.com>
2022-11-15 01:42:03 -0600

    Update app/upgrades/v12/constants.go
    Co-authored-by: Udit Gulati <85029005+udit-gulati@users.noreply.github.com>

--- app/upgrades/v12/constants.go
+++ app/upgrades/v12/constants.go
@@ -6,7 +6,7 @@ import (
 	store "github.com/cosmos/cosmos-sdk/store/types"
 )
 
-// UpgradeName defines the on-chain upgrade name for the Juno v11 upgrade.
+// UpgradeName defines the on-chain upgrade name for the Juno v12 upgrade.
 const UpgradeName = "v12"
 
 var Upgrade = upgrades.Upgrade{
