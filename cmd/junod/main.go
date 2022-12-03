6432d55609472994b0b16fb7b248c2b4cc5eba99
Jacob Gadikian <jacobgadikian@gmail.com>
2022-12-03 18:17:55 +0700

    gives Juno the canonical cmd folder structure

--- cmd/junod/main.go
+++ cmd/junod/main.go
@@ -4,13 +4,14 @@ import (
 	"os"
 
 	"github.com/CosmosContracts/juno/v12/app"
+	"github.com/CosmosContracts/juno/v12/cmd/junod/cmd"
 	"github.com/cosmos/cosmos-sdk/server"
 
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 )
 
 func main() {
-	rootCmd, _ := NewRootCmd()
+	rootCmd, _ := cmd.NewRootCmd()
 	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
 		switch e := err.(type) {
 		case server.ErrorCode:
