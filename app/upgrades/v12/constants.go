cfa06e7a775c39b3326f80f6e1dbaa3e1cfb2a6a
Reece Williams <reecepbcups@gmail.com>
2022-12-03 00:21:44 -0600

    store migration

--- app/upgrades/v12/constants.go
+++ app/upgrades/v12/constants.go
@@ -4,6 +4,7 @@ import (
 	tokenfactorytypes "github.com/CosmWasm/token-factory/x/tokenfactory/types"
 	"github.com/CosmosContracts/juno/v12/app/upgrades"
 	store "github.com/cosmos/cosmos-sdk/store/types"
+	ibcfeetypes "github.com/cosmos/ibc-go/v4/modules/apps/transfer/types"
 )
 
 // UpgradeName defines the on-chain upgrade name for the Juno v12 upgrade.
@@ -13,6 +14,6 @@ var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateV12UpgradeHandler,
 	StoreUpgrades: store.StoreUpgrades{
-		Added: []string{tokenfactorytypes.ModuleName},
+		Added: []string{tokenfactorytypes.ModuleName, ibcfeetypes.ModuleName},
 	},
 }
