469f08a60febccf6af280216df2dbcea603e43c6
Jacob Gadikian <jacobgadikian@gmail.com>
2022-12-08 15:43:46 +0700

    Update bech32.go

--- cmd/junod/cmd/bech32.go
+++ cmd/junod/cmd/bech32.go
@@ -9,7 +9,7 @@ import (
 
 var flagBech32Prefix = "prefix"
 
-// get cmd to convert any bech32 address to an juno prefix.
+// get cmd to convert any bech32 address to a juno prefix.
 func ConvertBech32Cmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "bech32-convert [bech32 string]",
