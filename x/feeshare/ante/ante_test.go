81e07cce4a4e23dd2b56a8b1f57842bd03823033
Reece Williams <reecepbcups@gmail.com>
2022-12-08 17:12:55 -0600

    Multiple fee test

--- x/feeshare/ante/ante_test.go
+++ x/feeshare/ante/ante_test.go
@@ -20,90 +20,96 @@ func TestAnteSuite(t *testing.T) {
 func (suite *AnteTestSuite) TestFeeLogic() {
 
 	// We expect all to pass
-	feeCoins := sdk.NewCoins(sdk.NewCoin("ujuno", sdk.NewInt(500)))
+	feeCoins := sdk.NewCoins(sdk.NewCoin("ujuno", sdk.NewInt(500)), sdk.NewCoin("utoken", sdk.NewInt(250)))
 
 	testCases := []struct {
 		name               string
 		incomingFee        sdk.Coins
 		govPercent         sdk.Dec
 		numContracts       int
-		expectedFeePayment sdk.Coin
+		expectedFeePayment sdk.Coins
 	}{
 		{
-			"100% fee",
+			"100% fee / 1 contract",
 			feeCoins,
 			sdk.NewDecWithPrec(100, 2),
 			1,
-			sdk.NewCoin("ujuno", sdk.NewInt(500)),
+			sdk.NewCoins(sdk.NewCoin("ujuno", sdk.NewInt(500)), sdk.NewCoin("utoken", sdk.NewInt(250))),
 		},
 		{
-			"100% fee / 2 contracts @ 500 fee = 250 per",
+			"100% fee / 2 contracts",
 			feeCoins,
 			sdk.NewDecWithPrec(100, 2),
 			2,
-			sdk.NewCoin("ujuno", sdk.NewInt(250)),
+			sdk.NewCoins(sdk.NewCoin("ujuno", sdk.NewInt(250)), sdk.NewCoin("utoken", sdk.NewInt(125))),
 		},
 		{
-			"100% fee / 10 contracts @ 500 fee = 50 per",
+			"100% fee / 10 contracts",
 			feeCoins,
 			sdk.NewDecWithPrec(100, 2),
 			10,
-			sdk.NewCoin("ujuno", sdk.NewInt(50)),
+			sdk.NewCoins(sdk.NewCoin("ujuno", sdk.NewInt(50)), sdk.NewCoin("utoken", sdk.NewInt(25))),
 		},
 		{
-			"67% fee / 7 contracts @ 500 fee = 48 per",
+			"67% fee / 7 contracts",
 			feeCoins,
 			sdk.NewDecWithPrec(67, 2),
 			7,
-			sdk.NewCoin("ujuno", sdk.NewInt(48)),
+			sdk.NewCoins(sdk.NewCoin("ujuno", sdk.NewInt(48)), sdk.NewCoin("utoken", sdk.NewInt(24))),
 		},
 		{
-			"50% fee / 1 contracts @ 500 fee = 250 per",
+			"50% fee / 1 contracts",
 			feeCoins,
 			sdk.NewDecWithPrec(50, 2),
 			1,
-			sdk.NewCoin("ujuno", sdk.NewInt(250)),
+			sdk.NewCoins(sdk.NewCoin("ujuno", sdk.NewInt(250)), sdk.NewCoin("utoken", sdk.NewInt(125))),
 		},
 		{
-			"50% fee / 2 contracts @ 500 fee = 125 per",
+			"50% fee / 2 contracts",
 			feeCoins,
 			sdk.NewDecWithPrec(50, 2),
 			2,
-			sdk.NewCoin("ujuno", sdk.NewInt(125)),
+			sdk.NewCoins(sdk.NewCoin("ujuno", sdk.NewInt(125)), sdk.NewCoin("utoken", sdk.NewInt(62))),
 		},
 		{
-			"50% fee / 3 contracts @ 500 fee = 83 per",
+			"50% fee / 3 contracts",
 			feeCoins,
 			sdk.NewDecWithPrec(50, 2),
 			3,
-			sdk.NewCoin("ujuno", sdk.NewInt(83)),
+			sdk.NewCoins(sdk.NewCoin("ujuno", sdk.NewInt(83)), sdk.NewCoin("utoken", sdk.NewInt(42))),
 		},
 		{
-			"25% fee / 2 contracts @ 500 fee = 62 per",
+			"25% fee / 2 contracts",
 			feeCoins,
 			sdk.NewDecWithPrec(25, 2),
 			2,
-			sdk.NewCoin("ujuno", sdk.NewInt(62)),
+			sdk.NewCoins(sdk.NewCoin("ujuno", sdk.NewInt(62)), sdk.NewCoin("utoken", sdk.NewInt(31))),
 		},
 		{
-			"15% fee / 3 contracts @ 500 fee = 25 per",
+			"15% fee / 3 contracts",
 			feeCoins,
 			sdk.NewDecWithPrec(15, 2),
 			3,
-			sdk.NewCoin("ujuno", sdk.NewInt(25)),
+			sdk.NewCoins(sdk.NewCoin("ujuno", sdk.NewInt(25)), sdk.NewCoin("utoken", sdk.NewInt(12))),
 		},
 		{
-			"1% fee / 2 contracts @ 500 fee = 0 per",
+			"1% fee / 2 contracts",
 			feeCoins,
 			sdk.NewDecWithPrec(1, 2),
 			2,
-			sdk.NewCoin("ujuno", sdk.NewInt(2)),
+			sdk.NewCoins(sdk.NewCoin("ujuno", sdk.NewInt(2)), sdk.NewCoin("utoken", sdk.NewInt(1))),
 		},
 	}
 
 	for _, tc := range testCases {
 		coins := ante.FeePayLogic(tc.incomingFee, tc.govPercent, tc.numContracts)
 
-		suite.Require().Equal(tc.expectedFeePayment.Amount.Int64(), coins[0].Amount.Int64(), tc.name)
+		for _, coin := range coins {
+			for _, expectedCoin := range tc.expectedFeePayment {
+				if coin.Denom == expectedCoin.Denom {
+					suite.Require().Equal(expectedCoin.Amount.Int64(), coin.Amount.Int64(), tc.name)
+				}
+			}
+		}
 	}
 }
