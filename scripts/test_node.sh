4a56be89f6f5bba09463d35117d01c5e4f3e244a
Jacob Gadikian <jacobgadikian@gmail.com>
2022-12-21 20:38:12 +0700

    satisfy codacy?

--- scripts/test_node.sh
+++ scripts/test_node.sh
@@ -7,45 +7,50 @@ CHAINID="juno-t1"
 MONIKER="localjuno"
 KEYALGO="secp256k1"
 KEYRING="test"
-LOGLjunoL="info"
 
 junod config keyring-backend $KEYRING
 junod config chain-id $CHAINID
 
-command -v junod > /dev/null 2>&1 || { echo >&2 "junod command not found. Ensure this is setup / properly installed in your GOPATH."; exit 1; }
-command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
+command -v junod >/dev/null 2>&1 || {
+  echo >&2 "junod command not found. Ensure this is setup / properly installed in your GOPATH."
+  exit 1
+}
+command -v jq >/dev/null 2>&1 || {
+  echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"
+  exit 1
+}
 
-from_scratch () {
+from_scratch() {
 
   make install
 
   # remove existing daemon.
-  rm -rf ~/.juno/* 
-  
+  rm -rf ~/.juno/*
+
   # juno1efd63aw40lxf3n4mhf7dzhjkr453axurv2zdzk
   echo "decorate bright ozone fork gallery riot bus exhaust worth way bone indoor calm squirrel merry zero scheme cotton until shop any excess stage laundry" | junod keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO --recover
   # juno1hj5fveer5cjtn4wd6wstzugjfdxzl0xps73ftl
   echo "wealth flavor believe regret funny network recall kiss grape useless pepper cram hint member few certain unveil rather brick bargain curious require crowd raise" | junod keys add feeacc --keyring-backend $KEYRING --algo $KEYALGO --recover
-  
-  junod init $MONIKER --chain-id $CHAINID 
+
+  junod init $MONIKER --chain-id $CHAINID
 
   # Function updates the config based on a jq argument as a string
-  update_test_genesis () {
+  update_test_genesis() {
     # update_test_genesis '.consensus_params["block"]["max_gas"]="100000000"'
-    cat $HOME/.juno/config/genesis.json | jq "$1" > $HOME/.juno/config/tmp_genesis.json && mv $HOME/.juno/config/tmp_genesis.json $HOME/.juno/config/genesis.json
+    cmd <"$HOME"/.juno/config/genesis.json | jq "$1" >"$HOME"/.juno/config/tmp_genesis.json && mv "$HOME"/.juno/config/tmp_genesis.json "$HOME"/.juno/config/genesis.json
   }
 
   # Set gas limit in genesis
   update_test_genesis '.consensus_params["block"]["max_gas"]="100000000"'
   update_test_genesis '.app_state["gov"]["voting_params"]["voting_period"]="15s"'
 
-  update_test_genesis '.app_state["staking"]["params"]["bond_denom"]="ujuno"'  
+  update_test_genesis '.app_state["staking"]["params"]["bond_denom"]="ujuno"'
   update_test_genesis '.app_state["bank"]["params"]["send_enabled"]=[{"denom": "ujuno","enabled": true}]'
-  # update_test_genesis '.app_state["staking"]["params"]["min_commission_rate"]="0.100000000000000000"' # sdk 46 only   
+  # update_test_genesis '.app_state["staking"]["params"]["min_commission_rate"]="0.100000000000000000"' # sdk 46 only
 
-  update_test_genesis '.app_state["mint"]["params"]["mint_denom"]="ujuno"'  
+  update_test_genesis '.app_state["mint"]["params"]["mint_denom"]="ujuno"'
   update_test_genesis '.app_state["gov"]["deposit_params"]["min_deposit"]=[{"denom": "ujuno","amount": "1000000"}]'
-  update_test_genesis '.app_state["crisis"]["constant_fee"]={"denom": "ujuno","amount": "1000"}'  
+  update_test_genesis '.app_state["crisis"]["constant_fee"]={"denom": "ujuno","amount": "1000"}'
 
   update_test_genesis '.app_state["tokenfactory"]["params"]["denom_creation_fee"]=[{"denom":"ujuno","amount":"100"}]'
 
@@ -64,8 +69,7 @@ from_scratch () {
   junod validate-genesis
 }
 
-
-if [ $# -eq 1 ] && [ $1 == "clean" ] || [ $1 == "c" ]; then
+if [ $# -eq 1 ] && [ "$1" == "clean" ] || [ "$1" == "c" ]; then
   echo "Starting from a clean state"
   from_scratch
 fi
@@ -77,4 +81,4 @@ sed -i '/laddr = "tcp:\/\/127.0.0.1:26657"/c\laddr = "tcp:\/\/0.0.0.0:26657"' ~/
 sed -i 's/cors_allowed_origins = \[\]/cors_allowed_origins = \["\*"\]/g' ~/.juno/config/config.toml
 sed -i 's/enable = false/enable = true/g' ~/.juno/config/app.toml
 
-junod start --pruning=nothing  --minimum-gas-prices=0ujuno  
+junod start --pruning=nothing --minimum-gas-prices=0ujuno
