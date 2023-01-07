9c9257faf2974d8b06199b8e8cc47073a7358d72
Reece Williams <reecepbcups@gmail.com>
2023-01-07 13:09:04 -0600

    Script - Easily run local oracle with test_node.sh

---
+++ scripts/oracle/run_local_oracle.sh
@@ -0,0 +1,36 @@
+# first `sh ./scripts/test_node.sh clean`
+# then in a new tab:
+# sh ./scripts/oracle/run_local_oracle.sh
+
+ORACLE_FILENAME="test_oracle"
+
+cd price-feeder
+make install
+
+price-feeder version
+
+cp config.toml $HOME/.juno/$ORACLE_FILENAME.toml
+
+# replace gas_price
+sed -i 's/0.0001stake/0.025ujuno/g' ~/.juno/$ORACLE_FILENAME.toml
+
+# replace feeder address
+sed -i 's/address = "juno1w20tfhnehc33rgtm9tg8gdtea0svn7twfnyqee"/address = "juno1hj5fveer5cjtn4wd6wstzugjfdxzl0xps73ftl"/g' ~/.juno/$ORACLE_FILENAME.toml
+
+# change chain_id
+sed -i 's/chain_id = "test-1"/chain_id = "juno-t1"/g' ~/.juno/$ORACLE_FILENAME.toml
+sed -i 's/"chain_id", "test-1"/"chain_id", "juno-t1"/g' ~/.juno/$ORACLE_FILENAME.toml
+
+# change dir to home juno
+sed -i 's/dir = "\/Users\/vuong\/.juno"/dir = "\~\/.juno"/g' ~/.juno/$ORACLE_FILENAME.toml
+
+VALOPER_ADDR=$(junod q staking validators --node http://localhost:26657 --output json | jq -r '.validators[0].operator_address')
+
+# change validator
+sed -i "s/validator = \"junovaloper1w20tfhnehc33rgtm9tg8gdtea0svn7twkwj0zq\"/validator = \"$VALOPER_ADDR\"/g" ~/.juno/$ORACLE_FILENAME.toml
+
+# change 'websocket
+sed -i 's/websocket = "stream.binance.com:9443"/websocket = "fstream.binance.com:443"/g' ~/.juno/$ORACLE_FILENAME.toml
+
+# start it 
+price-feeder $HOME/.juno/$ORACLE_FILENAME.toml
