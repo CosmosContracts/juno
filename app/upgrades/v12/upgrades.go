706498a05a4680ebbd963a54b142530cbbfe07e9
Reece Williams <reecepbcups@gmail.com>
2023-01-08 15:56:39 -0600

    fix atom denom precision (4 -> 3)

--- app/upgrades/v12/upgrades.go
+++ app/upgrades/v12/upgrades.go
@@ -142,7 +142,7 @@ func CreateV12UpgradeHandler(
 			// 0.0025ujuno
 			sdk.NewDecCoinFromDec(nativeDenom, sdk.NewDecWithPrec(25, 4)),
 			// 0.001 ATOM CHANNEL-1 -> `junod q ibc-transfer denom-trace ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9`
-			sdk.NewDecCoinFromDec("ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9", sdk.NewDecWithPrec(1, 4)),
+			sdk.NewDecCoinFromDec("ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9", sdk.NewDecWithPrec(1, 3)),
 		}
 		s, ok := keepers.ParamsKeeper.GetSubspace(globalfeetypes.ModuleName)
 		if !ok {
