d97013662b0254050e9662674179831344969e00
Reece Williams <reecepbcups@gmail.com>
2023-01-20 19:15:24 -0600

    update, better funcs

--- scripts/test_upgrade.sh
+++ scripts/test_upgrade.sh
@@ -5,15 +5,16 @@
 # BINARY=junodv11 NEW_BINARY=junod TIMEOUT_COMMIT="5000ms" CLEAN=true sh ./scripts/test_upgrade.sh
 #
 # VALOPER_ADDR=junovaloper1efd63aw40lxf3n4mhf7dzhjkr453axurnh5ze0 FEEDER_ADDRESS=juno1efd63aw40lxf3n4mhf7dzhjkr453axurv2zdzk JUNO_DIR="~/.juno1/" sh ./scripts/oracle/run_local_oracle.sh
-# VALOPER_ADDR=junovaloper1hj5fveer5cjtn4wd6wstzugjfdxzl0xp0r8xsx FEEDER_ADDRESS=juno1hj5fveer5cjtn4wd6wstzugjfdxzl0xps73ftl LISTEN_ADDR="0.0.0.0:7172" JUNO_DIR="~/.juno-2/" sh ./scripts/oracle/run_local_oracle.sh
+# VALOPER_ADDR=junovaloper1hj5fveer5cjtn4wd6wstzugjfdxzl0xp0r8xsx FEEDER_ADDRESS=juno1hj5fveer5cjtn4wd6wstzugjfdxzl0xps73ftl LISTEN_ADDR="0.0.0.0:7172" JUNO_DIR="~/.juno2/" sh ./scripts/oracle/run_local_oracle.sh
 
 export KEY="juno1"
 export CHAIN_ID=${CHAIN_ID:-"local-1"}
-export MONIKER="localjuno"
 export KEYALGO="secp256k1"
 export KEYRING=${KEYRING:-"test"}
+
 export HOME_DIR=$(eval echo "${HOME_DIR:-"~/.juno1/"}")
-export HOME_DIRB=$(eval echo "${HOME_DIRB:-"~/.juno-2/"}")
+export HOME_DIRB=$(eval echo "${HOME_DIRB:-"~/.juno2/"}")
+export HOME_DIRC=$(eval echo "${HOME_DIRC:-"~/.juno3/"}")
 
 export BINARY=${BINARY:-junodv11}
 export NEW_BINARY=${NEW_BINARY:-junod}
@@ -21,6 +22,7 @@ export CLEAN=${CLEAN:-"false"}
 
 export TIMEOUT_COMMIT=${TIMEOUT_COMMIT:-"5s"}
 
+# Validator 1
 export RPC=${RPC:-"26657"}
 export REST=${REST:-"1317"}
 export PROFF=${PROFF:-"6060"}
@@ -29,6 +31,7 @@ export GRPC=${GRPC:-"9090"}
 export GRPC_WEB=${GRPC_WEB:-"9091"}
 export ROSETTA=${ROSETTA:-"8080"}
 
+# Validator 2
 export RPCB=${RPCB:-"36657"}
 export RESTB=${RESTB:-"2317"}
 export PROFFB=${PROFFB:-"6061"}
@@ -37,19 +40,42 @@ export GRPCB=${GRPCB:-"8090"}
 export GRPC_WEBB=${GRPC_WEBB:-"8091"}
 export ROSETTAB=${ROSETTAB:-"8081"}
 
+# Validator 3
+export RPCC=${RPCC:-"46657"}
+export RESTC=${RESTC:-"3317"}
+export PROFFC=${PROFFC:-"7061"}
+export P2PC=${P2PC:-"46656"}
+export GRPCC=${GRPCC:-"8097"}
+export GRPC_WEBC=${GRPC_WEBC:-"8098"}
+export ROSETTAC=${ROSETTAC:-"8089"}
+
 # Kill all instances before starting back up
 killall $BINARY
 killall $NEW_BINARY
 
-junod config keyring-backend $KEYRING
-junod config chain-id $CHAIN_ID
-
 # Debugging
-echo "CHAIN_ID=$CHAIN_ID, HOME_DIR=$HOME_DIR, CLEAN=$CLEAN, RPC=$RPC, REST=$REST, PROFF=$PROFF, P2P=$P2P, GRPC=$GRPC, GRPC_WEB=$GRPC_WEB, ROSETTA=$ROSETTA, TIMEOUT_COMMIT=$TIMEOUT_COMMIT"
-echo "CHAIN_ID=$CHAIN_ID, HOME_DIR=$HOME_DIRB, CLEAN=$CLEAN, RPC=$RPC2, REST=$REST2, PROFF=$PROFF2, P2P=$P2P2, GRPC=$GRPC2, GRPC_WEB=$GRPC_WEB2, ROSETTA=$ROSETTA2, TIMEOUT_COMMIT=$TIMEOUT_COMMIT"
+# echo "CHAIN_ID=$CHAIN_ID, HOME_DIR=$HOME_DIR, CLEAN=$CLEAN, RPC=$RPC, REST=$REST, PROFF=$PROFF, P2P=$P2P, GRPC=$GRPC, GRPC_WEB=$GRPC_WEB, ROSETTA=$ROSETTA, TIMEOUT_COMMIT=$TIMEOUT_COMMIT"
+# echo "CHAIN_ID=$CHAIN_ID, HOME_DIR=$HOME_DIRB, CLEAN=$CLEAN, RPC=$RPC2, REST=$REST2, PROFF=$PROFF2, P2P=$P2P2, GRPC=$GRPC2, GRPC_WEB=$GRPC_WEB2, ROSETTA=$ROSETTA2, TIMEOUT_COMMIT=$TIMEOUT_COMMIT"
 
 command -v junod > /dev/null 2>&1 || { echo >&2 "junod command not found. Ensure this is setup / properly installed in your GOPATH."; exit 1; }
 command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
+
+add_keys () {
+  NAME=$1
+  WORDS=$2
+  # add to all home dirs
+  echo "$WORDS" | $BINARY --home=$HOME_DIR keys add $NAME --keyring-backend $KEYRING --algo $KEYALGO --recover
+  echo "$WORDS" | $BINARY --home=$HOME_DIRB keys add $NAME --keyring-backend $KEYRING --algo $KEYALGO --recover
+  # echo "$WORDS" | $BINARY --home=$HOME_DIRC keys add $NAME --keyring-backend $KEYRING --algo $KEYALGO --recover
+}
+
+add_balances () {
+  ADDRESS=$1
+
+  $BINARY --home=$HOME_DIR add-genesis-account $ADDRESS 10000000ujuno,1000utest --keyring-backend $KEYRING
+  $BINARY --home=$HOME_DIRB add-genesis-account $ADDRESS 10000000ujuno,1000utest --keyring-backend $KEYRING
+  # $BINARY --home=$HOME_DIRC add-genesis-account $ADDRESS 10000000ujuno,1000utest --keyring-backend $KEYRING
+}
 
 from_scratch () {
   # Fresh install on current branch
@@ -58,16 +84,20 @@ from_scratch () {
   # remove existing daemon.
   rm -rf $HOME_DIR && echo "Removed $HOME_DIR"  
   rm -rf $HOME_DIRB && echo "Removed $HOME_DIRB"  
-  
-  # juno1efd63aw40lxf3n4mhf7dzhjkr453axurv2zdzk
-  echo "decorate bright ozone fork gallery riot bus exhaust worth way bone indoor calm squirrel merry zero scheme cotton until shop any excess stage laundry" | $BINARY --home=$HOME_DIR keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO --recover
-  echo "decorate bright ozone fork gallery riot bus exhaust worth way bone indoor calm squirrel merry zero scheme cotton until shop any excess stage laundry" | $BINARY --home=$HOME_DIRB keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO --recover
-  # juno1hj5fveer5cjtn4wd6wstzugjfdxzl0xps73ftl
-  echo "wealth flavor believe regret funny network recall kiss grape useless pepper cram hint member few certain unveil rather brick bargain curious require crowd raise" | $BINARY --home=$HOME_DIR keys add feeacc --keyring-backend $KEYRING --algo $KEYALGO --recover
-  echo "wealth flavor believe regret funny network recall kiss grape useless pepper cram hint member few certain unveil rather brick bargain curious require crowd raise" | $BINARY --home=$HOME_DIRB keys add feeacc --keyring-backend $KEYRING --algo $KEYALGO --recover
-  
-  $BINARY --home=$HOME_DIR init $MONIKER --chain-id $CHAIN_ID
-  $BINARY --home=$HOME_DIRB init "other" --chain-id $CHAIN_ID
+  # rm -rf $HOME_DIRC && echo "Removed $HOME_DIRC"    
+
+  $BINARY --home=$HOME_DIR init "ahh" --chain-id $CHAIN_ID
+  $BINARY --home=$HOME_DIRB init "bee" --chain-id $CHAIN_ID
+  # $BINARY --home=$HOME_DIRC init "cee" --chain-id $CHAIN_ID
+
+  # juno1efd63aw40lxf3n4mhf7dzhjkr453axurv2zdzk  
+  add_keys "juno1" "decorate bright ozone fork gallery riot bus exhaust worth way bone indoor calm squirrel merry zero scheme cotton until shop any excess stage laundry"
+
+  # juno1hj5fveer5cjtn4wd6wstzugjfdxzl0xps73ftl  
+  add_keys "feeacc" "wealth flavor believe regret funny network recall kiss grape useless pepper cram hint member few certain unveil rather brick bargain curious require crowd raise"
+
+  # juno1g20vre3x9l35rwterkrfw47kyhgypzm5ezewjd
+  add_keys "val3" "stable echo above noise tooth master dilemma defense water boost mirror witness quick emotion napkin crowd purity cabbage survey stomach story bounce cake become"  
 
   # Function updates the config based on a jq argument as a string
   update_test_genesis () {
@@ -91,25 +121,24 @@ from_scratch () {
 
   update_test_genesis '.app_state["feeshare"]["params"]["allowed_denoms"]=["ujuno"]'
 
-
   # Allocate genesis accounts
-  $BINARY --home=$HOME_DIR add-genesis-account $KEY 10000000ujuno,1000utest --keyring-backend $KEYRING
-  $BINARY --home=$HOME_DIR add-genesis-account juno1efd63aw40lxf3n4mhf7dzhjkr453axurv2zdzk 10000000ujuno,1000utest --keyring-backend $KEYRING
-  $BINARY --home=$HOME_DIRB add-genesis-account feeacc 10000000ujuno,1000utest --keyring-backend $KEYRING  
-
-  # some random account
-  $BINARY --home=$HOME_DIRB add-genesis-account juno12smx2wdlyttvyzvzg54y2vnqwq2qjatezqwqxu 10000000ujuno,1000utest --keyring-backend $KEYRING  
-
-  # since we copy anyways
-  # /home/reece/.juno/config/gentx/gentx-5eb04a48391716aee064ed4d1245b14ddd7997ad.json
-  # TODO; allow ip to change via config variable for public access
+  add_balances juno1efd63aw40lxf3n4mhf7dzhjkr453axurv2zdzk
+  add_balances juno1hj5fveer5cjtn4wd6wstzugjfdxzl0xps73ftl
+  add_balances juno1g20vre3x9l35rwterkrfw47kyhgypzm5ezewjd    
+    
+  # Gentxs
   $BINARY --home=$HOME_DIR gentx $KEY 1000000ujuno --keyring-backend $KEYRING --chain-id $CHAIN_ID --home $HOME_DIR --ip 127.0.0.1
-  BINARY_1_PEER=$($BINARY tendermint show-node-id --home $HOME_DIR)
-  # ls $HOME_DIR/config/gentx
+  BINARY_1_PEER=$($BINARY tendermint show-node-id --home $HOME_DIR) && echo "$BINARY_1_PEER"  
 
+  # 2.
   $BINARY gentx feeacc 1000000ujuno --keyring-backend $KEYRING --chain-id $CHAIN_ID --home $HOME_DIRB --ip 127.0.0.1
   BINARY_2_PEER=$($BINARY tendermint show-node-id --home $HOME_DIRB) && echo "$BINARY_2_PEER"
   cp $HOME_DIRB/config/gentx/*.json $HOME_DIR/config/gentx/gentx-other.json  
+
+  # 3.
+  # $BINARY gentx val3 1000000ujuno --keyring-backend $KEYRING --chain-id $CHAIN_ID --home $HOME_DIRC --ip 127.0.0.1
+  # BINARY_3_PEER=$($BINARY tendermint show-node-id --home $HOME_DIRC) && echo "$BINARY_3_PEER"
+  # cp $HOME_DIRC/config/gentx/*.json $HOME_DIR/config/gentx/gentx-other_2.json  
 
   # Collect genesis tx
   $BINARY --home=$HOME_DIR collect-gentxs --home $HOME_DIR
@@ -117,8 +146,9 @@ from_scratch () {
   # Run this to ensure junorything worked and that the genesis file is setup correctly
   $BINARY --home=$HOME_DIR validate-genesis
 
-  # copy it from the first node to the second
+  # copy it from the first node to the second & 3rd  
   cp $HOME_DIR/config/genesis.json $HOME_DIRB/config/genesis.json
+  # cp $HOME_DIR/config/genesis.json $HOME_DIRC/config/genesis.json
 }
 
 # check if CLEAN is not set to false
@@ -127,62 +157,60 @@ if [ "$CLEAN" != "false" ]; then
   from_scratch
 fi
 
-# exit 1
-
-# echo "Starting node..."
-
-# FIRST FOLDER
-# Opens the RPC endpoint to outside connections
-sed -i 's/laddr = "tcp:\/\/127.0.0.1:26657"/c\laddr = "tcp:\/\/0.0.0.0:'$RPC'"/g' $HOME_DIR/config/config.toml
-sed -i 's/cors_allowed_origins = \[\]/cors_allowed_origins = \["\*"\]/g' $HOME_DIR/config/config.toml
-# REST endpoint
-sed -i 's/address = "tcp:\/\/0.0.0.0:1317"/address = "tcp:\/\/0.0.0.0:'$REST'"/g' $HOME_DIR/config/app.toml
-sed -i 's/enable = false/enable = true/g' $HOME_DIR/config/app.toml
-# replace pprof_laddr = "localhost:6060" binding
-sed -i 's/pprof_laddr = "localhost:6060"/pprof_laddr = "localhost:'$PROFF_LADDER'"/g' $HOME_DIR/config/config.toml
-# change p2p addr laddr = "tcp://0.0.0.0:26656"
-sed -i 's/laddr = "tcp:\/\/0.0.0.0:26656"/laddr = "tcp:\/\/0.0.0.0:'$P2P'"/g' $HOME_DIR/config/config.toml
-# GRPC
-sed -i 's/address = "0.0.0.0:9090"/address = "0.0.0.0:'$GRPC'"/g' $HOME_DIR/config/app.toml
-sed -i 's/address = "0.0.0.0:9091"/address = "0.0.0.0:'$GRPC_WEB'"/g' $HOME_DIR/config/app.toml
-# Rosetta Api
-sed -i 's/address = ":8080"/address = "0.0.0.0:'$ROSETTA'"/g' $HOME_DIR/config/app.toml
-# pruning (fix panic)
-sed -i 's/pruning = "default"/pruning = "nothing"/g' $HOME_DIR/config/app.toml
-# faster blocks
-sed -i 's/timeout_commit = "5s"/timeout_commit = "'$TIMEOUT_COMMIT'"/g' $HOME_DIR/config/config.toml
 
 
-# SECOND FOLDER
-sed -i 's/laddr = "tcp:\/\/127.0.0.1:26657"/c\laddr = "tcp:\/\/0.0.0.0:'$RPCB'"/g' $HOME_DIRB/config/config.toml
-sed -i 's/cors_allowed_origins = \[\]/cors_allowed_origins = \["\*"\]/g' $HOME_DIRB/config/config.toml
-# REST endpoint
-sed -i 's/address = "tcp:\/\/0.0.0.0:1317"/address = "tcp:\/\/0.0.0.0:'$RESTB'"/g' $HOME_DIRB/config/app.toml
-sed -i 's/enable = false/enable = true/g' $HOME_DIRB/config/app.toml
-# replace pprof_laddr = "localhost:6060" binding
-sed -i 's/pprof_laddr = "localhost:6060"/pprof_laddr = "localhost:'$PROFF_LADDERB'"/g' $HOME_DIRB/config/config.toml
-# change p2p addr laddr = "tcp://0.0.0.0:26656"
-sed -i 's/laddr = "tcp:\/\/0.0.0.0:26656"/laddr = "tcp:\/\/0.0.0.0:'$P2PB'"/g' $HOME_DIRB/config/config.toml
-# GRPC
-sed -i 's/address = "0.0.0.0:9090"/address = "0.0.0.0:'$GRPCB'"/g' $HOME_DIRB/config/app.toml
-sed -i 's/address = "0.0.0.0:9091"/address = "0.0.0.0:'$GRPC_WEBB'"/g' $HOME_DIRB/config/app.toml
-# Rosetta Api
-sed -i 's/address = ":8080"/address = "0.0.0.0:'$ROSETTAB'"/g' $HOME_DIRB/config/app.toml
-# pruning (fix panic)
-sed -i 's/pruning = "default"/pruning = "nothing"/g' $HOME_DIRB/config/app.toml
 
-# faster blocks
-sed -i 's/timeout_commit = "5s"/timeout_commit = "'$TIMEOUT_COMMIT'"/g' $HOME_DIRB/config/config.toml
+update_config () {
+  JUNO_DIR=$1
+  tmpRPC=$2
+  tmpREST=$3
+  tmpPROFF_LADDER=$4
+  tmpP2P=$5
+  tmpGRPC=$6
+  tmpGRPC_WEB=$7
+  tmpROSETTA=$8
 
-# # Start the node with 0 gas fees
-$BINARY start --home $HOME_DIR --pruning=nothing  --minimum-gas-prices=0ujuno --rpc.laddr="tcp://0.0.0.0:$RPC" --p2p.persistent_peers "$BINARY_2_PEER"@127.0.0.1:$P2PB &
+  CONFIG=$JUNO_DIR/config/config.toml
+  APP=$JUNO_DIR/config/app.toml  
 
-# start with peer of the other
-$BINARY start --home $HOME_DIRB --pruning=nothing  --minimum-gas-prices=0ujuno --rpc.laddr="tcp://0.0.0.0:$RPCB" --p2p.persistent_peers "$BINARY_1_PEER"@127.0.0.1:$P2P --p2p.laddr="tcp://0.0.0.0:$P2PB" --grpc-web.enable=false --grpc.address="0.0.0.0:$GRPCB" &
+  # Opens the RPC endpoint to outside connections
+  sed -i 's/laddr = "tcp:\/\/127.0.0.1:26657"/c\laddr = "tcp:\/\/0.0.0.0:'$tmpRPC'"/g' $CONFIG
+  sed -i 's/cors_allowed_origins = \[\]/cors_allowed_origins = \["\*"\]/g' $CONFIG
+
+  # REST endpoint
+  sed -i 's/address = "tcp:\/\/0.0.0.0:1317"/address = "tcp:\/\/0.0.0.0:'$tmpREST'"/g' $APP
+  sed -i 's/enable = false/enable = true/g' $APP
+
+  # replace pprof_laddr = "localhost:6060" binding
+  sed -i 's/pprof_laddr = "localhost:6060"/pprof_laddr = "localhost:'$tmpPROFF_LADDER'"/g' $CONFIG
+
+  # change p2p addr laddr = "tcp://0.0.0.0:26656"
+  sed -i 's/laddr = "tcp:\/\/0.0.0.0:26656"/laddr = "tcp:\/\/0.0.0.0:'$tmpP2P'"/g' $CONFIG
+
+  # GRPC
+  sed -i 's/address = "0.0.0.0:9090"/address = "0.0.0.0:'$tmpGRPC'"/g' $APP
+  sed -i 's/address = "0.0.0.0:9091"/address = "0.0.0.0:'$tmpGRPC_WEB'"/g' $APP
+
+  # Rosetta Api
+  sed -i 's/address = ":8080"/address = "0.0.0.0:'$tmpROSETTA'"/g' $APP  
+
+  # faster blocks
+  sed -i 's/timeout_commit = "5s"/timeout_commit = "'$TIMEOUT_COMMIT'"/g' $CONFIG
+}
+
+# update all configs to their respective ports
+update_config "$HOME_DIR" "$RPC" "$REST" "$PROFF_LADDER" "$P2P" "$GRPC" "$GRPC_WEB" "$ROSETTA"
+update_config "$HOME_DIRB" "$RPCB" "$RESTB" "$PROFF_LADDERB" "$P2PB" "$GRPCB" "$GRPC_WEBB" "$ROSETTAB"
+# update_config "$HOME_DIRC" "$RPCC" "$RESTC" "$PROFF_LADDERC" "$P2PC" "$GRPCC" "$GRPC_WEBC" "$ROSETTAC"
+
+# Start Nodes
+DEFAULT_START_FLAGS="--grpc-web.enable=false --pruning=default --minimum-gas-prices=0ujuno"
+$BINARY start --home $HOME_DIR $DEFAULT_START_FLAGS --rpc.laddr="tcp://0.0.0.0:$RPC" --p2p.persistent_peers "$BINARY_2_PEER"@127.0.0.1:$P2PB &
+$BINARY start --home $HOME_DIRB $DEFAULT_START_FLAGS --rpc.laddr="tcp://0.0.0.0:$RPCB" --p2p.persistent_peers "$BINARY_1_PEER"@127.0.0.1:$P2P --p2p.laddr="tcp://0.0.0.0:$P2PB" --grpc.address="0.0.0.0:$GRPCB" &
+# $BINARY start --home $HOME_DIRC --pruning=default --minimum-gas-prices=0ujuno --rpc.laddr="tcp://0.0.0.0:$RPCC" --p2p.persistent_peers "$BINARY_2_PEER"@127.0.0.1:$P2PB --p2p.laddr="tcp://0.0.0.0:$P2PC" --grpc-web.enable=false --grpc.address="0.0.0.0:$GRPCC" &
 
 sleep 15
 
-# submit prop tio vote and halt eventually
 echo -e "\n\n\n\nSUBMIT PROPOSAL"
 $BINARY tx gov submit-proposal software-upgrade v12 --title "v12 upgrade test" --description "test upgrade" --deposit 1000000ujuno --upgrade-height 7 --from $KEY --keyring-backend test --home $HOME_DIR --chain-id $CHAIN_ID --yes --broadcast-mode block
 echo -e "\n\n\nVOTE"
@@ -200,21 +228,3 @@ echo -e "\n\n\nSTART NEW"
 # # Start the nodes again
 $NEW_BINARY start --home $HOME_DIR --pruning=nothing  --minimum-gas-prices=0ujuno --rpc.laddr="tcp://0.0.0.0:$RPC" --p2p.persistent_peers "$BINARY_2_PEER"@127.0.0.1:$P2PB &
 $NEW_BINARY start --home $HOME_DIRB --pruning=nothing  --minimum-gas-prices=0ujuno --rpc.laddr="tcp://0.0.0.0:$RPCB" --p2p.persistent_peers "$BINARY_1_PEER"@127.0.0.1:$P2P --p2p.laddr="tcp://0.0.0.0:$P2PB" --grpc-web.enable=false --grpc.address="0.0.0.0:$GRPCB" &
-
-# start the price feeder with this info?
-
-# doUpgrade() {
-#   # Run this block manually in your terminal
-
-#   # Waitm then we will run these after it halts at height  
-
-  
-#   # junod q globalfee minimum-gas-prices --node http://localhost:26657
-
-#   $NEWBINARY tx gov submit-proposal param-change ~/Desktop/Work/paramChange.json --from $KEY --keyring-backend $KEYRING --chain-id $CHAINID --yes --broadcast-mode block --node http://localhost:26657 --fees 600ujuno
-#   ID="1"
-#   $NEWBINARY tx gov vote $ID yes --from $KEY --keyring-backend $KEYRING --chain-id $CHAINID --yes --broadcast-mode block
-#   $NEWBINARY q gov proposal $ID
-
-#   junod tx bank send $KEY juno1efd63aw40lxf3n4mhf7dzhjkr453axurv2zdzk 1ujuno --keyring-backend $KEYRING --chain-id $CHAINID --yes --broadcast-mode block --fees 500ujuno --gas 150000
-# }
