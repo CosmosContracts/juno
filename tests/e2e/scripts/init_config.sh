6e531a44d0166b981273b754402f93b9006b42ea
ducnt87 <ntd1312000@gmail.com>
2023-02-01 16:40:21 +0700

    init_config

--- tests/e2e/scripts/init_config.sh
+++ tests/e2e/scripts/init_config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sed -i '114,117d' config.toml
-address=`sed '230!d' /tmp/juno-e2e-testnet/juno-test-a/juno-test-a-node-prune-default-snapshot-state-sync-from/config/genesis.json`
-validator=`sed '231!d' /tmp/juno-e2e-testnet/juno-test-a/juno-test-a-node-prune-default-snapshot-state-sync-from/config/genesis.json`
+address=`sed '214!d' /tmp/juno-e2e-testnet/juno-test-a/juno-test-a-node-prune-default-snapshot-state-sync-from/config/genesis.json`
+validator=`sed '215!d' /tmp/juno-e2e-testnet/juno-test-a/juno-test-a-node-prune-default-snapshot-state-sync-from/config/genesis.json`
 var1=${address#*juno}
 var2=${var1%\"*}
 var3=${validator#*juno}
