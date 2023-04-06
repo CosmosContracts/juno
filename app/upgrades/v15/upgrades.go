ac3844671f51e0ac5b1c02a88bdf266f4f216bf8
Reece Williams <reecepbcups@gmail.com>
2023-04-05 21:00:04 -0500

    v15 upgrade handler

---
+++ app/upgrades/v15/upgrades.go
@@ -0,0 +1,41 @@
+package v15
+
+import (
+	"fmt"
+
+	"github.com/CosmosContracts/juno/v15/app/keepers"
+
+	"github.com/CosmosContracts/juno/v15/app/upgrades"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+func CreateV15UpgradeHandler(
+	mm *module.Manager,
+	cfg module.Configurator,
+	keepers *keepers.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		logger := ctx.Logger().With("upgrade", UpgradeName)
+
+		nativeDenom := upgrades.GetChainsDenomToken(ctx.ChainID())
+		logger.Info(fmt.Sprintf("With native denom %s", nativeDenom))
+
+		logger.Info(fmt.Sprintf("pre migrate version map: %v", vm))
+
+		// Run migrations
+		versionMap, err := mm.RunMigrations(ctx, cfg, vm)
+
+		logger.Info(fmt.Sprintf("post migrate version map: %v", versionMap))
+
+		// Double blocks per year (from 6 seconds to 3 = 2x blocks per year)
+		mintParams := keepers.MintKeeper.GetParams(ctx)
+		mintParams.BlocksPerYear = mintParams.BlocksPerYear * 2
+		keepers.MintKeeper.SetParams(ctx, mintParams)
+		logger.Info(fmt.Sprintf("updated minted blocks per year logic to %v", mintParams.BlocksPerYear))
+
+		return versionMap, err
+	}
+}
