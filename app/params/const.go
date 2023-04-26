1dc59d799d61852134b80cfab2b5fd3313dc4922
Lặc <expertdicer@gmail.com>
2023-04-26 14:57:25 +0700

    add token factory default weights

--- app/params/const.go
+++ app/params/const.go
@@ -13,4 +13,11 @@ const (
 	// DefaultGasLimit - set to the same value as cosmos-sdk flags.DefaultGasLimit
 	// this value is currently only used in tests.
 	DefaultGasLimit = 200000
+
+	DefaultWeightMsgCreateDenom      int = 100
+	DefaultWeightMsgMint             int = 100
+	DefaultWeightMsgBurn             int = 100
+	DefaultWeightMsgChangeAdmin      int = 100
+	DefaultWeightMsgSetDenomMetadata int = 100
+	DefaultWeightMsgForceTransfer    int = 100
 )
