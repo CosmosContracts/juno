a5d2e637f6a4c0a790f550eae7e4c978876746e7
Lặc <expertdicer@gmail.com>
2023-04-27 11:47:07 +0700

    bump osmoutils

--- osmoutils/noapptest/ctx.go
+++ osmoutils/noapptest/ctx.go
@@ -6,17 +6,18 @@ import (
 	"github.com/cometbft/cometbft/libs/log"
 	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
 	"github.com/cosmos/cosmos-sdk/store"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	dbm "github.com/tendermint/tm-db"
+	dbm "github.com/cometbft/cometbft-db"
 )
 
-func CtxWithStoreKeys(keys []sdk.StoreKey, header tmproto.Header, isCheckTx bool) sdk.Context {
+func CtxWithStoreKeys(keys []storetypes.StoreKey, header tmproto.Header, isCheckTx bool) sdk.Context {
 	db := dbm.NewMemDB()
 	logger := log.NewNopLogger()
-	cms := store.NewCommitMultiStore(db, logger)
+	cms := store.NewCommitMultiStore(db)
 	for _, key := range keys {
-		cms.MountStoreWithDB(key, sdk.StoreTypeIAVL, nil)
+		cms.MountStoreWithDB(key, storetypes.StoreTypeIAVL, nil)
 	}
 	err := cms.LoadLatestVersion()
 	if err != nil {
@@ -25,7 +26,7 @@ func CtxWithStoreKeys(keys []sdk.StoreKey, header tmproto.Header, isCheckTx bool
 	return sdk.NewContext(cms, header, isCheckTx, logger)
 }
 
-func DefaultCtxWithStoreKeys(storeKeys []sdk.StoreKey) sdk.Context {
+func DefaultCtxWithStoreKeys(storeKeys []storetypes.StoreKey) sdk.Context {
 	header := tmproto.Header{Height: 1, ChainID: "osmoutils-test-1", Time: time.Now().UTC()}
 	deliverTx := false
 	return CtxWithStoreKeys(storeKeys, header, deliverTx)
