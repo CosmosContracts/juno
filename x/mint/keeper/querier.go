42170c1e276db7c56c279df00cbc49932ae1ff5a
Reece Williams <reecepbcups@gmail.com>
2023-04-28 18:57:45 -0500

    Comment out legacy x/mint querier

--- x/mint/keeper/querier.go
+++ x/mint/keeper/querier.go
@@ -1,62 +1,65 @@
 package keeper
 
-import (
-	abci "github.com/cometbft/cometbft/abci/types"
+// TODO: Can not find anything in the docs which says we need this? the Route() was removed and seems this was apart of that.
+// If so it does nothing now.
 
-	"github.com/CosmosContracts/juno/v15/x/mint/types"
-	"github.com/cosmos/cosmos-sdk/codec"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-)
+// import (
+// 	abci "github.com/cometbft/cometbft/abci/types"
 
-// NewQuerier returns a minting Querier handler.
-func NewQuerier(k Keeper, legacyQuerierCdc *codec.LegacyAmino) sdk.Querier {
-	return func(ctx sdk.Context, path []string, _ abci.RequestQuery) ([]byte, error) {
-		switch path[0] {
-		case types.QueryParameters:
-			return queryParams(ctx, k, legacyQuerierCdc)
+// 	"github.com/CosmosContracts/juno/v15/x/mint/types"
+// 	"github.com/cosmos/cosmos-sdk/codec"
+// 	sdk "github.com/cosmos/cosmos-sdk/types"
+// 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+// )
 
-		case types.QueryInflation:
-			return queryInflation(ctx, k, legacyQuerierCdc)
+// // NewQuerier returns a minting Querier handler.
+// func NewQuerier(k Keeper, legacyQuerierCdc *codec.LegacyAmino) sdk.Querier {
+// 	return func(ctx sdk.Context, path []string, _ abci.RequestQuery) ([]byte, error) {
+// 		switch path[0] {
+// 		case types.QueryParameters:
+// 			return queryParams(ctx, k, legacyQuerierCdc)
 
-		case types.QueryAnnualProvisions:
-			return queryAnnualProvisions(ctx, k, legacyQuerierCdc)
+// 		case types.QueryInflation:
+// 			return queryInflation(ctx, k, legacyQuerierCdc)
 
-		default:
-			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unknown query path: %s", path[0])
-		}
-	}
-}
+// 		case types.QueryAnnualProvisions:
+// 			return queryAnnualProvisions(ctx, k, legacyQuerierCdc)
 
-func queryParams(ctx sdk.Context, k Keeper, legacyQuerierCdc *codec.LegacyAmino) ([]byte, error) {
-	params := k.GetParams(ctx)
+// 		default:
+// 			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unknown query path: %s", path[0])
+// 		}
+// 	}
+// }
 
-	res, err := codec.MarshalJSONIndent(legacyQuerierCdc, params)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
-	}
+// func queryParams(ctx sdk.Context, k Keeper, legacyQuerierCdc *codec.LegacyAmino) ([]byte, error) {
+// 	params := k.GetParams(ctx)
 
-	return res, nil
-}
+// 	res, err := codec.MarshalJSONIndent(legacyQuerierCdc, params)
+// 	if err != nil {
+// 		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+// 	}
 
-func queryInflation(ctx sdk.Context, k Keeper, legacyQuerierCdc *codec.LegacyAmino) ([]byte, error) {
-	minter := k.GetMinter(ctx)
+// 	return res, nil
+// }
 
-	res, err := codec.MarshalJSONIndent(legacyQuerierCdc, minter.Inflation)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
-	}
+// func queryInflation(ctx sdk.Context, k Keeper, legacyQuerierCdc *codec.LegacyAmino) ([]byte, error) {
+// 	minter := k.GetMinter(ctx)
 
-	return res, nil
-}
+// 	res, err := codec.MarshalJSONIndent(legacyQuerierCdc, minter.Inflation)
+// 	if err != nil {
+// 		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+// 	}
 
-func queryAnnualProvisions(ctx sdk.Context, k Keeper, legacyQuerierCdc *codec.LegacyAmino) ([]byte, error) {
-	minter := k.GetMinter(ctx)
+// 	return res, nil
+// }
 
-	res, err := codec.MarshalJSONIndent(legacyQuerierCdc, minter.AnnualProvisions)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
-	}
+// func queryAnnualProvisions(ctx sdk.Context, k Keeper, legacyQuerierCdc *codec.LegacyAmino) ([]byte, error) {
+// 	minter := k.GetMinter(ctx)
 
-	return res, nil
-}
+// 	res, err := codec.MarshalJSONIndent(legacyQuerierCdc, minter.AnnualProvisions)
+// 	if err != nil {
+// 		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+// 	}
+
+// 	return res, nil
+// }
