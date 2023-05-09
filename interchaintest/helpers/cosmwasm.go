060512af55985efe3905d1ba1edda333af3af208
Reece Williams <reecepbcups@gmail.com>
2023-05-09 16:21:05 -0500

    Adds FeeShare interchain test

--- interchaintest/helpers/cosmwasm.go
+++ interchaintest/helpers/cosmwasm.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"testing"
 
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	"github.com/strangelove-ventures/interchaintest/v4/chain/cosmos"
+	"github.com/strangelove-ventures/interchaintest/v4/ibc"
+	"github.com/strangelove-ventures/interchaintest/v4/testutil"
+	"github.com/stretchr/testify/require"
 )
 
 func SetupContract(t *testing.T, ctx context.Context, chain *cosmos.CosmosChain, keyname string, fileLoc string, message string) (codeId, contract string) {
@@ -23,3 +27,59 @@ func SetupContract(t *testing.T, ctx context.Context, chain *cosmos.CosmosChain,
 	return codeId, contractAddr
 }
 
+func ExecuteMsgWithAmount(t *testing.T, ctx context.Context, chain *cosmos.CosmosChain, user *ibc.Wallet, contractAddr, amount, message string) {
+	// amount is #utoken
+
+	// There has to be a way to do this in ictest?
+	cmd := []string{"junod", "tx", "wasm", "execute", contractAddr, message,
+		"--node", chain.GetRPCAddress(),
+		"--home", chain.HomeDir(),
+		"--chain-id", chain.Config().ChainID,
+		"--from", user.KeyName,
+		"--gas", "500000",
+		"--amount", amount,
+		"--keyring-dir", chain.HomeDir(),
+		"--keyring-backend", keyring.BackendTest,
+		"-y",
+	}
+	_, _, err := chain.Exec(ctx, cmd, nil)
+	require.NoError(t, err)
+
+	// t.Log("msg", cmd)
+	// t.Log("ExecuteMsgWithAmount", string(stdout))
+
+	if err := testutil.WaitForBlocks(ctx, 2, chain); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func ExecuteMsgWithFee(t *testing.T, ctx context.Context, chain *cosmos.CosmosChain, user *ibc.Wallet, contractAddr, amount, feeCoin, message string) {
+	// amount is #utoken
+
+	// There has to be a way to do this in ictest?
+	cmd := []string{"junod", "tx", "wasm", "execute", contractAddr, message,
+		"--node", chain.GetRPCAddress(),
+		"--home", chain.HomeDir(),
+		"--chain-id", chain.Config().ChainID,
+		"--from", user.KeyName,
+		"--gas", "500000",
+		"--fees", feeCoin,
+		"--keyring-dir", chain.HomeDir(),
+		"--keyring-backend", keyring.BackendTest,
+		"-y",
+	}
+
+	if amount != "" {
+		cmd = append(cmd, "--amount", amount)
+	}
+
+	_, _, err := chain.Exec(ctx, cmd, nil)
+	require.NoError(t, err)
+
+	// t.Log("msg", cmd)
+	// t.Log("ExecuteMsgWithAmount", string(stdout))
+
+	if err := testutil.WaitForBlocks(ctx, 2, chain); err != nil {
+		t.Fatal(err)
+	}
+}
