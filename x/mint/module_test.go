c37f4fbe43348084a8ba76faeb9aca1bd19b5849
Reece Williams <reecepbcups@gmail.com>
2023-05-10 20:59:04 -0500

    Fix x/mint MOduleAccount test

--- x/mint/module_test.go
+++ x/mint/module_test.go
@@ -3,25 +3,23 @@ package mint_test
 import (
 	"testing"
 
-	abcitypes "github.com/cometbft/cometbft/abci/types"
 	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
 	"github.com/stretchr/testify/require"
 
-	"github.com/CosmosContracts/juno/v15/app"
 	"github.com/CosmosContracts/juno/v15/x/mint/types"
+	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/cosmos/cosmos-sdk/x/nft/testutil"
 )
 
 func TestItCreatesModuleAccountOnInitBlock(t *testing.T) {
-	app := app.Setup(t)
+	var accountKeeper authkeeper.AccountKeeper
+
+	app, err := simtestutil.SetupAtGenesis(testutil.AppConfig, &accountKeeper)
+	require.NoError(t, err)
+
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
-
-	app.InitChain(
-		abcitypes.RequestInitChain{
-			AppStateBytes: []byte("{}"),
-		},
-	)
-
-	acc := app.AppKeepers.AccountKeeper.GetAccount(ctx, authtypes.NewModuleAddress(types.ModuleName))
+	acc := accountKeeper.GetAccount(ctx, authtypes.NewModuleAddress(types.ModuleName))
 	require.NotNil(t, acc)
 }
