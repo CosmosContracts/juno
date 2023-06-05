8ee78cd1fbc0d0e40b8c263cb3c2f70408b4108c
Reece Williams <reecepbcups@gmail.com>
2023-06-05 16:43:06 -0500

    comment

--- x/mint/keeper/keeper.go
+++ x/mint/keeper/keeper.go
@@ -108,17 +108,6 @@ func (k Keeper) GetParams(ctx sdk.Context) (p types.Params) {
 	return p
 }
 
-// GetParams returns the total set of minting parameters.
-// func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
-// 	k.paramSpace.GetParamSet(ctx, &params)
-// 	return params
-// }
-
-// // SetParams sets the total set of minting parameters.
-// func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
-// 	k.paramSpace.SetParamSet(ctx, &params)
-// }
-
 // ______________________________________________________________________
 
 // StakingTokenSupply implements an alias call to the underlying staking keeper's
