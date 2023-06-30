fa74eb1f4de86e93d5478d40c77894ef400cebbf
Reece Williams <reecepbcups@gmail.com>
2023-06-29 23:33:23 -0500

    adds linter & auto tidy

---
+++ scripts/git/tidy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+go mod tidy -v
+if [ $? -ne 0 ]; then
+  echo "go mod tidy failed"
+  exit 2
+fi
+
+git diff --exit-code go.* &> /dev/null
+if [ $? -ne 0 ]; then
+    echo "go.mod or go.sum differs, please re-add it to your commit"
+    exit 3
+fi
