9267aea5543f19b259cf8ba3598b668259669cff
Reece Williams <reecepbcups@gmail.com>
2023-08-02 14:24:17 -0500

    typo

--- scripts/test_cwmodules.sh
+++ scripts/test_cwmodules.sh
@@ -13,7 +13,7 @@ export KEY2="juno2"
 export JUNOD_NODE="http://localhost:26657"
 
 # SudoMsg of JunoEndBlocker
-junod tx wasm store ./sciprts/cwmodules_example.wasm --from $KEY --gas 5000000 --chain-id $CHAIN_ID --gas-prices 0.025ujuno --node $JUNOD_NODE --broadcast-mode sync --yes
+junod tx wasm store ./scripts/cwmodules_example.wasm --from $KEY --gas 5000000 --chain-id $CHAIN_ID --gas-prices 0.025ujuno --node $JUNOD_NODE --broadcast-mode sync --yes
 CODE_ID=1
 
 junod tx wasm instantiate "1" "{}" --from $KEY --gas 5000000 --chain-id $CHAIN_ID --gas-prices 0.025ujuno --node $JUNOD_NODE --broadcast-mode sync --yes --label="A" --no-admin
