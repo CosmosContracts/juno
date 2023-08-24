da2e82de7cf54650c90cbf205794e5b0d64e962e
Reece Williams <reecepbcups@gmail.com>
2023-08-23 20:59:17 -0500

    forced linting

--- x/tokenfactory/keeper/msg_server_test.go
+++ x/tokenfactory/keeper/msg_server_test.go
@@ -238,6 +238,7 @@ func (suite *KeeperTestSuite) TestSetDenomMetaDataMsg() {
 		},
 	} {
 		suite.Run(fmt.Sprintf("Case %s", tc.desc), func() {
+			tc := tc
 			ctx := suite.Ctx.WithEventManager(sdk.NewEventManager())
 			suite.Require().Equal(0, len(ctx.EventManager().Events()))
 			// Test set denom metadata message
