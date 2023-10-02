84a465ab737765dcfb6a71f9498e84d8412d4072
Reece Williams <reecepbcups@gmail.com>
2023-10-01 19:54:51 -0500

    staking: new contract with all methods

--- x/cw-hooks/script.sh
+++ x/cw-hooks/script.sh
@@ -4,7 +4,7 @@ export JUNOD_NODE="tcp://localhost:26657"
 
 FLAGS="--from=juno1 --gas=2500000 --fees=50000ujuno --node=http://localhost:26657 --yes --keyring-backend=test --home $HOME/.juno1 --chain-id=local-1 --output=json"
 
-junod tx wasm store ./contract/juno_staking_hooks_example.wasm $FLAGS
+junod tx wasm store ./keeper/contract/juno_staking_hooks_example.wasm $FLAGS
 
 sleep 5
 
@@ -20,7 +20,8 @@ junod q cw-hooks staking-contracts
 junod q wasm contract-state smart $addr '{"get_config":{}}' --node=http://localhost:26657
 
 # get last validator
-junod q wasm contract-state smart $addr '{"get_last_val_info":{}}' --node=http://localhost:26657
+junod q wasm contract-state smart $addr '{"last_val_change":{}}' --node=http://localhost:26657
+junod q wasm contract-state smart $addr '{"last_delegation_change":{}}' --node=http://localhost:26657
 
 # create validator
 junod tx staking create-validator --amount 1ujuno --commission-rate="0.05" --commission-max-rate="1.0" --commission-max-change-rate="1.0" --moniker="test123" --from=juno2 --pubkey=$(junod tendermint show-validator --home $HOME/.juno) --min-self-delegation="1" --gas=1000000 --fees=50000ujuno --node=http://localhost:26657 --yes --keyring-backend=test --home $HOME/.juno1 --chain-id=local-1 --output=json
