d890568b7a45db43d02ac727603c67ddacb7a951
Joel Smith <joelsmith.2019@gmail.com>
2023-10-06 15:34:02 -0500

    Add Genesis Tests

--- x/feepay/genesis_test.go
+++ x/feepay/genesis_test.go
@@ -1,3 +1,83 @@
 package feepay_test
 
-// x/feeshare
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/CosmosContracts/juno/v17/app"
+	"github.com/CosmosContracts/juno/v17/x/feepay"
+	"github.com/CosmosContracts/juno/v17/x/feepay/types"
+)
+
+type GenesisTestSuite struct {
+	suite.Suite
+
+	ctx sdk.Context
+
+	app     *app.App
+	genesis types.GenesisState
+}
+
+func TestGenesisTestSuite(t *testing.T) {
+	suite.Run(t, new(GenesisTestSuite))
+}
+
+func (suite *GenesisTestSuite) SetupTest() {
+	app := app.Setup(suite.T())
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{
+		ChainID: "testing",
+	})
+
+	suite.app = app
+	suite.ctx = ctx
+
+	suite.genesis = *types.DefaultGenesisState()
+}
+
+func (suite *GenesisTestSuite) TestFeeShareInitGenesis() {
+	testCases := []struct {
+		name     string
+		genesis  types.GenesisState
+		expPanic bool
+	}{
+		{
+			"Default Genesis - FeePay Enabled",
+			suite.genesis,
+			false,
+		},
+		{
+			"Custom Genesis - FeePay Disabled",
+			types.GenesisState{
+				Params: types.Params{
+					EnableFeepay: false,
+				},
+			},
+			false,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.name), func() {
+			suite.SetupTest() // reset
+
+			if tc.expPanic {
+				suite.Require().Panics(func() {
+					feepay.InitGenesis(suite.ctx, suite.app.AppKeepers.FeePayKeeper, tc.genesis)
+				})
+			} else {
+				suite.Require().NotPanics(func() {
+					feepay.InitGenesis(suite.ctx, suite.app.AppKeepers.FeePayKeeper, tc.genesis)
+				})
+
+				params := suite.app.AppKeepers.FeePayKeeper.GetParams(suite.ctx)
+				suite.Require().Equal(tc.genesis.Params, params)
+			}
+		})
+	}
+}
