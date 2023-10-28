36593b112ebc0f0be4fab85bf777413d8955ce0d
Reece Williams <reecepbcups@gmail.com>
2023-10-28 13:03:45 -0500

    v1800alpha2 upgrade

--- app/app.go
+++ app/app.go
@@ -66,6 +66,7 @@ import (
 	"github.com/CosmosContracts/juno/v18/app/keepers"
 	"github.com/CosmosContracts/juno/v18/app/openapiconsole"
 	upgrades "github.com/CosmosContracts/juno/v18/app/upgrades"
+	testnetV18alpha2 "github.com/CosmosContracts/juno/v18/app/upgrades/testnet/v18.0.0-alpha.2"
 	v10 "github.com/CosmosContracts/juno/v18/app/upgrades/v10"
 	v11 "github.com/CosmosContracts/juno/v18/app/upgrades/v11"
 	v12 "github.com/CosmosContracts/juno/v18/app/upgrades/v12"
@@ -97,6 +98,9 @@ var (
 	EnableSpecificProposals = ""
 
 	Upgrades = []upgrades.Upgrade{
+		// testnet
+		testnetV18alpha2.Upgrade,
+
 		v10.Upgrade,
 		v11.Upgrade,
 		v12.Upgrade,
