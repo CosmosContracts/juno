45fee2000b19c0c77508e5a79d150189657ff828
Joel Smith <joelsmith.2019@gmail.com>
2023-11-27 15:00:42 -0600

    Test Msg Server

--- x/clock/keeper/keeper_test.go
+++ x/clock/keeper/keeper_test.go
@@ -1,14 +1,19 @@
 package keeper_test
 
 import (
+	"crypto/sha256"
 	"testing"
 	"time"
 
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	"github.com/stretchr/testify/suite"
+
+	_ "embed"
 
 	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
@@ -56,4 +61,77 @@ func (s *IntegrationTestSuite) FundAccount(ctx sdk.Context, addr sdk.AccAddress,
 
 func TestKeeperTestSuite(t *testing.T) {
 	suite.Run(t, new(IntegrationTestSuite))
+}
+
+//go:embed testdata/clock_example.wasm
+var wasmContract []byte
+
+func (s *IntegrationTestSuite) StoreCode() {
+	_, _, sender := testdata.KeyTestPubAddr()
+	msg := wasmtypes.MsgStoreCodeFixture(func(m *wasmtypes.MsgStoreCode) {
+		m.WASMByteCode = wasmContract
+		m.Sender = sender.String()
+	})
+	rsp, err := s.app.MsgServiceRouter().Handler(msg)(s.ctx, msg)
+	s.Require().NoError(err)
+	var result wasmtypes.MsgStoreCodeResponse
+	s.Require().NoError(s.app.AppCodec().Unmarshal(rsp.Data, &result))
+	s.Require().Equal(uint64(1), result.CodeID)
+	expHash := sha256.Sum256(wasmContract)
+	s.Require().Equal(expHash[:], result.Checksum)
+	// and
+	info := s.app.AppKeepers.WasmKeeper.GetCodeInfo(s.ctx, 1)
+	s.Require().NotNil(info)
+	s.Require().Equal(expHash[:], info.CodeHash)
+	s.Require().Equal(sender.String(), info.Creator)
+	s.Require().Equal(wasmtypes.DefaultParams().InstantiateDefaultPermission.With(sender), info.InstantiateConfig)
+}
+
+func (s *IntegrationTestSuite) InstantiateContract(sender string, admin string) string {
+	msgStoreCode := wasmtypes.MsgStoreCodeFixture(func(m *wasmtypes.MsgStoreCode) {
+		m.WASMByteCode = wasmContract
+		m.Sender = sender
+	})
+	_, err := s.app.MsgServiceRouter().Handler(msgStoreCode)(s.ctx, msgStoreCode)
+	s.Require().NoError(err)
+
+	msgInstantiate := wasmtypes.MsgInstantiateContractFixture(func(m *wasmtypes.MsgInstantiateContract) {
+		m.Sender = sender
+		m.Admin = admin
+		m.Msg = []byte(`{}`)
+	})
+	resp, err := s.app.MsgServiceRouter().Handler(msgInstantiate)(s.ctx, msgInstantiate)
+	s.Require().NoError(err)
+	var result wasmtypes.MsgInstantiateContractResponse
+	s.Require().NoError(s.app.AppCodec().Unmarshal(resp.Data, &result))
+	contractInfo := s.app.AppKeepers.WasmKeeper.GetContractInfo(s.ctx, sdk.MustAccAddressFromBech32(result.Address))
+	s.Require().Equal(contractInfo.CodeID, uint64(1))
+	s.Require().Equal(contractInfo.Admin, admin)
+	s.Require().Equal(contractInfo.Creator, sender)
+
+	return result.Address
+}
+
+// Helper method for quickly registering a clock contract
+func (s *IntegrationTestSuite) RegisterClockContract(senderAddress string, contractAddress string) {
+	err := s.app.AppKeepers.ClockKeeper.RegisterContract(s.ctx, senderAddress, contractAddress)
+	s.Require().NoError(err)
+}
+
+// Helper method for quickly unregistering a clock contract
+func (s *IntegrationTestSuite) UnregisterClockContract(senderAddress string, contractAddress string) {
+	err := s.app.AppKeepers.ClockKeeper.UnregisterContract(s.ctx, senderAddress, contractAddress)
+	s.Require().NoError(err)
+}
+
+// Helper method for quickly jailing a clock contract
+func (s *IntegrationTestSuite) JailClockContract(contractAddress string) {
+	err := s.app.AppKeepers.ClockKeeper.JailContract(s.ctx, contractAddress)
+	s.Require().NoError(err)
+}
+
+// Helper method for quickly unjailing a clock contract
+func (s *IntegrationTestSuite) UnjailClockContract(senderAddress string, contractAddress string) {
+	err := s.app.AppKeepers.ClockKeeper.UnjailContract(s.ctx, senderAddress, contractAddress)
+	s.Require().NoError(err)
 }
--- x/clock/keeper/msg_server_test.go
+++ x/clock/keeper/msg_server_test.go
@@ -4,68 +4,266 @@ import (
 	_ "embed"
 
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/CosmosContracts/juno/v18/x/clock/types"
 )
 
-func (s *IntegrationTestSuite) TestUpdateClockParams() {
+// Test register clock contract.
+func (s *IntegrationTestSuite) TestRegisterClockContract() {
 	_, _, addr := testdata.KeyTestPubAddr()
 	_, _, addr2 := testdata.KeyTestPubAddr()
+	_ = s.FundAccount(s.ctx, addr, sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(1_000_000))))
+
+	// Store code
+	s.StoreCode()
+	contractAddress := s.InstantiateContract(addr.String(), "")
 
 	for _, tc := range []struct {
-		desc              string
-		isEnabled         bool
-		ContractAddresses []string
-		success           bool
+		desc     string
+		sender   string
+		contract string
+		isJailed bool
+		success  bool
 	}{
 		{
-			desc:              "Success - Valid on",
-			isEnabled:         true,
-			ContractAddresses: []string{},
-			success:           true,
+			desc:     "Success - Register Contract",
+			sender:   addr.String(),
+			contract: contractAddress,
+			success:  true,
 		},
 		{
-			desc:              "Success - Valid off",
-			isEnabled:         false,
-			ContractAddresses: []string{},
-			success:           true,
+			desc:     "Error - Invalid Sender",
+			sender:   addr2.String(),
+			contract: contractAddress,
+			success:  false,
 		},
 		{
-			desc:              "Success - On and 1 allowed address",
-			isEnabled:         true,
-			ContractAddresses: []string{addr.String()},
-			success:           true,
+			desc:     "Fail - Invalid Contract Address",
+			sender:   addr.String(),
+			contract: "Invalid",
+			success:  false,
 		},
 		{
-			desc:              "Fail - On and 2 duplicate addresses",
-			isEnabled:         true,
-			ContractAddresses: []string{addr.String(), addr.String()},
-			success:           false,
+			desc:     "Fail - Invalid Sender Address",
+			sender:   "Invalid",
+			contract: contractAddress,
+			success:  false,
 		},
 		{
-			desc:              "Success - On and 2 unique",
-			isEnabled:         true,
-			ContractAddresses: []string{addr.String(), addr2.String()},
-			success:           true,
-		},
-		{
-			desc:              "Success - On and 2 duplicate 1 unique",
-			isEnabled:         true,
-			ContractAddresses: []string{addr.String(), addr2.String(), addr.String()},
-			success:           false,
+			desc:     "Fail - Contract Already Jailed",
+			sender:   addr.String(),
+			contract: contractAddress,
+			isJailed: true,
+			success:  false,
 		},
 	} {
 		tc := tc
 		s.Run(tc.desc, func() {
+			// Set params
 			params := types.DefaultParams()
-			// params.ContractAddresses = tc.ContractAddresses
-
 			err := s.app.AppKeepers.ClockKeeper.SetParams(s.ctx, params)
+			s.Require().NoError(err)
+
+			// Jail contract if needed
+			if tc.isJailed {
+				err := s.app.AppKeepers.ClockKeeper.JailContract(s.ctx, contractAddress)
+				s.Require().NoError(err)
+			}
+
+			// Try to register contract
+			res, err := s.clockMsgServer.RegisterClockContract(s.ctx, &types.MsgRegisterClockContract{
+				SenderAddress:   tc.sender,
+				ContractAddress: tc.contract,
+			})
 
 			if !tc.success {
 				s.Require().Error(err)
 			} else {
 				s.Require().NoError(err)
+				s.Require().Equal(res, &types.MsgRegisterClockContractResponse{})
+			}
+		})
+	}
+}
+
+// Test standard unregistration of clock contracts.
+func (s *IntegrationTestSuite) TestUnregisterClockContract() {
+	_, _, addr := testdata.KeyTestPubAddr()
+	_, _, addr2 := testdata.KeyTestPubAddr()
+	_ = s.FundAccount(s.ctx, addr, sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(1_000_000))))
+
+	s.StoreCode()
+	contractAddress := s.InstantiateContract(addr.String(), "")
+
+	for _, tc := range []struct {
+		desc     string
+		sender   string
+		contract string
+		success  bool
+	}{
+		{
+			desc:     "Success - Unregister Contract",
+			sender:   addr.String(),
+			contract: contractAddress,
+			success:  true,
+		},
+		{
+			desc:     "Error - Invalid Sender",
+			sender:   addr2.String(),
+			contract: contractAddress,
+			success:  false,
+		},
+		{
+			desc:     "Fail - Invalid Contract Address",
+			sender:   addr.String(),
+			contract: "Invalid",
+			success:  false,
+		},
+		{
+			desc:     "Fail - Invalid Sender Address",
+			sender:   "Invalid",
+			contract: contractAddress,
+			success:  false,
+		},
+	} {
+		tc := tc
+		s.Run(tc.desc, func() {
+			// Ensure contract is unregistered, ignore unregister error
+			_ = s.app.AppKeepers.ClockKeeper.UnregisterContract(s.ctx, addr.String(), contractAddress)
+			s.RegisterClockContract(addr.String(), contractAddress)
+
+			// Set params
+			params := types.DefaultParams()
+			err := s.app.AppKeepers.ClockKeeper.SetParams(s.ctx, params)
+			s.Require().NoError(err)
+
+			// Try to register all contracts
+			res, err := s.clockMsgServer.UnregisterClockContract(s.ctx, &types.MsgUnregisterClockContract{
+				SenderAddress:   tc.sender,
+				ContractAddress: tc.contract,
+			})
+
+			if !tc.success {
+				s.Require().Error(err)
+			} else {
+				s.Require().NoError(err)
+				s.Require().Equal(res, &types.MsgUnregisterClockContractResponse{})
+			}
+		})
+	}
+}
+
+// Test duplicate register/unregister clock contracts.
+func (s *IntegrationTestSuite) TestDuplicateRegistrationChecks() {
+	_, _, addr := testdata.KeyTestPubAddr()
+	_ = s.FundAccount(s.ctx, addr, sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(1_000_000))))
+
+	s.StoreCode()
+	contractAddress := s.InstantiateContract(addr.String(), "")
+
+	// Test double register, first succeed, second fail
+	_, err := s.clockMsgServer.RegisterClockContract(s.ctx, &types.MsgRegisterClockContract{
+		SenderAddress:   addr.String(),
+		ContractAddress: contractAddress,
+	})
+	s.Require().NoError(err)
+
+	_, err = s.clockMsgServer.RegisterClockContract(s.ctx, &types.MsgRegisterClockContract{
+		SenderAddress:   addr.String(),
+		ContractAddress: contractAddress,
+	})
+	s.Require().Error(err)
+
+	// Test double unregister, first succeed, second fail
+	_, err = s.clockMsgServer.UnregisterClockContract(s.ctx, &types.MsgUnregisterClockContract{
+		SenderAddress:   addr.String(),
+		ContractAddress: contractAddress,
+	})
+	s.Require().NoError(err)
+
+	_, err = s.clockMsgServer.UnregisterClockContract(s.ctx, &types.MsgUnregisterClockContract{
+		SenderAddress:   addr.String(),
+		ContractAddress: contractAddress,
+	})
+	s.Require().Error(err)
+}
+
+// Test unjailing clock contracts.
+func (s *IntegrationTestSuite) TestUnjailClockContract() {
+	_, _, addr := testdata.KeyTestPubAddr()
+	_, _, addr2 := testdata.KeyTestPubAddr()
+	_ = s.FundAccount(s.ctx, addr, sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(1_000_000))))
+
+	s.StoreCode()
+	contractAddress := s.InstantiateContract(addr.String(), "")
+	s.RegisterClockContract(addr.String(), contractAddress)
+
+	for _, tc := range []struct {
+		desc     string
+		sender   string
+		contract string
+		unjail   bool
+		success  bool
+	}{
+		{
+			desc:     "Success - Unjail Contract",
+			sender:   addr.String(),
+			contract: contractAddress,
+			success:  true,
+		},
+		{
+			desc:     "Error - Invalid Sender",
+			sender:   addr2.String(),
+			contract: contractAddress,
+			success:  false,
+		},
+		{
+			desc:     "Fail - Invalid Contract Address",
+			sender:   addr.String(),
+			contract: "Invalid",
+			success:  false,
+		},
+		{
+			desc:     "Fail - Invalid Sender Address",
+			sender:   "Invalid",
+			contract: contractAddress,
+			success:  false,
+		},
+		{
+			desc:     "Fail - Contract Not Jailed",
+			sender:   addr.String(),
+			contract: contractAddress,
+			unjail:   true,
+			success:  false,
+		},
+	} {
+		tc := tc
+		s.Run(tc.desc, func() {
+			_ = s.app.AppKeepers.ClockKeeper.UnjailContract(s.ctx, addr.String(), contractAddress)
+			s.JailClockContract(contractAddress)
+
+			// Unjail contract if needed
+			if tc.unjail {
+				s.UnjailClockContract(addr.String(), contractAddress)
+			}
+
+			// Set params
+			params := types.DefaultParams()
+			err := s.app.AppKeepers.ClockKeeper.SetParams(s.ctx, params)
+			s.Require().NoError(err)
+
+			// Try to register all contracts
+			res, err := s.clockMsgServer.UnjailClockContract(s.ctx, &types.MsgUnjailClockContract{
+				SenderAddress:   tc.sender,
+				ContractAddress: tc.contract,
+			})
+
+			if !tc.success {
+				s.Require().Error(err)
+			} else {
+				s.Require().NoError(err)
+				s.Require().Equal(res, &types.MsgUnjailClockContractResponse{})
 			}
 		})
 	}
