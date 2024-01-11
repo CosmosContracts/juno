7c073c8841b713027e82d0a1b6e1dc63fd06ca97
Reece Williams <reecepbcups@gmail.com>
2024-01-10 20:13:18 -0600

    init

--- app/upgrades/v19/upgrade_test.go
+++ app/upgrades/v19/upgrade_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/CosmosContracts/juno/v19/app/apptesting"
 	decorators "github.com/CosmosContracts/juno/v19/app/decorators"
 	v19 "github.com/CosmosContracts/juno/v19/app/upgrades/v19"
+	vestingtypes "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
 )
 
 type UpgradeTestSuite struct {
@@ -27,16 +28,37 @@ func TestKeeperTestSuite(t *testing.T) {
 	suite.Run(t, new(UpgradeTestSuite))
 }
 
+func (s *UpgradeTestSuite) setupMockCore1MultisigAccount() *vestingtypes.PeriodicVestingAccount {
+	core1Multisig := v19.CreateMainnetVestingAccount()
+	s.App.AppKeepers.AccountKeeper.SetAccount(s.Ctx, core1Multisig)
+	return core1Multisig
+}
+
 // Ensures the test does not error out.
 func (s *UpgradeTestSuite) TestUpgrade() {
 	s.Setup()
-
 	preUpgradeChecks(s)
 
+	// Core-1 account mock up
+	va := s.setupMockCore1MultisigAccount()
+	fmt.Println(va)
+
+	// delegate to a validator
+	valAddr1 := s.SetupValidator(stakingtypes.Bonded)
+	valAddr2 := s.SetupValidator(stakingtypes.Bonded)
+	valAddr3 := s.SetupValidator(stakingtypes.Bonded)
+
+	// delegate to a validator
+	s.DelegateToValidator(valAddr1, 1000000000)
+
+	// upgrade
 	upgradeHeight := int64(5)
 	s.ConfirmUpgradeSucceeded(v19.UpgradeName, upgradeHeight)
-
 	postUpgradeChecks(s)
+
+	// TODO: check it was modified
+	updatedAcc := s.App.AppKeepers.AccountKeeper.GetAccount(s.Ctx, va.GetAddress())
+	fmt.Println(updatedAcc)
 }
 
 func preUpgradeChecks(s *UpgradeTestSuite) {
@@ -74,4 +96,5 @@ func postUpgradeChecks(s *UpgradeTestSuite) {
 	for _, validator := range validators {
 		s.Require().True(validator.Commission.MaxChangeRate.LTE(sdk.MustNewDecFromStr(decorators.MaxChangeRate)))
 	}
+
 }
--- app/upgrades/v19/upgrades.go
+++ app/upgrades/v19/upgrades.go
@@ -2,6 +2,7 @@ package v19
 
 import (
 	"fmt"
+	"time"
 
 	wasmlctypes "github.com/cosmos/ibc-go/modules/light-clients/08-wasm/types"
 
@@ -12,6 +13,13 @@ import (
 	decorators "github.com/CosmosContracts/juno/v19/app/decorators"
 	"github.com/CosmosContracts/juno/v19/app/keepers"
 	"github.com/CosmosContracts/juno/v19/app/upgrades"
+	"github.com/cometbft/cometbft/libs/log"
+	vestingtypes "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
+)
+
+const (
+	Core1MultisigVestingAccount = "juno190g5j8aszqhvtg7cprmev8xcxs6csra7xnk3n3"
+	CharterCouncil              = "juno1nmezpepv3lx45mndyctz2lzqxa6d9xzd2xumkxf7a6r4nxt0y95qypm6c0"
 )
 
 func CreateV19UpgradeHandler(
@@ -45,6 +53,9 @@ func CreateV19UpgradeHandler(
 			}
 		}
 
+		// TODO: ONLY DO THIS WITH MAINNET
+		migrateCore1Vesting(ctx, logger, k)
+
 		// https://github.com/cosmos/ibc-go/blob/main/docs/docs/03-light-clients/04-wasm/03-integration.md
 		params := k.IBCKeeper.ClientKeeper.GetParams(ctx)
 		params.AllowedClients = append(params.AllowedClients, wasmlctypes.Wasm)
@@ -52,4 +63,35 @@ func CreateV19UpgradeHandler(
 
 		return versionMap, err
 	}
+}
+
+func migrateCore1Vesting(ctx sdk.Context, logger log.Logger, k *keepers.AppKeepers) {
+	core1Acc := k.AccountKeeper.GetAccount(ctx, sdk.MustAccAddressFromBech32(Core1MultisigVestingAccount))
+
+	vestingAcc, ok := core1Acc.(*vestingtypes.PeriodicVestingAccount)
+	if !ok {
+		panic(fmt.Errorf("core1Acc.(*vestingtypes.PeriodicVestingAccount): %+v", core1Acc))
+	}
+	fmt.Println(vestingAcc)
+
+	// remove 1 hour from the current block time to ensure it is set
+	currTime := ctx.BlockTime().Sub(time.Time{}.Add(time.Hour))
+	totalTokens := uint64(0)
+
+	vestingAcc.EndTime = int64(currTime.Seconds())
+
+	// TODO: remove all delegations instantly from prop16 code
+
+	// sum all tokens
+	for _, period := range vestingAcc.VestingPeriods {
+		for _, coin := range period.Amount {
+			if coin.Denom == "ujuno" {
+				totalTokens += coin.Amount.Uint64()
+			}
+		}
+	}
+
+	totalTokens += vestingAcc.DelegatedVesting[0].Amount.Uint64()
+
+	fmt.Println(totalTokens)
 }
