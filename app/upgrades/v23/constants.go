18621fbef44b255d5bd691b9699f14793872f06d
dimiandre <dimiandre@gmail.com>
2024-07-05 13:15:42 +0200

    remove icq from store upgrades

--- app/upgrades/v23/constants.go
+++ app/upgrades/v23/constants.go
@@ -2,7 +2,6 @@ package v23
 
 import (
 	store "github.com/cosmos/cosmos-sdk/store/types"
-	icqtypes "github.com/cosmos/ibc-apps/modules/async-icq/v7/types"
 
 	"github.com/CosmosContracts/juno/v23/app/upgrades"
 )
@@ -13,10 +12,5 @@ const UpgradeName = "v23"
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateV23UpgradeHandler,
-	StoreUpgrades:        store.StoreUpgrades{
-		Added: []string{
-			// updated modules
-			icqtypes.ModuleName,
-		},
-	},
+	StoreUpgrades:        store.StoreUpgrades{},
 }
