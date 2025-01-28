1023915e275d26dd28aea437d3f7644e1770580e
Marius Modlich <marius@modlich.de>
2025-01-28 19:10:00 +0100

    commented out previous migrations

--- x/mint/migrations/v3/migrate.go
+++ x/mint/migrations/v3/migrate.go
@@ -1,44 +1,44 @@
-package v2
+package v3
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"github.com/cosmos/cosmos-sdk/codec"
-	sdk "github.com/cosmos/cosmos-sdk/types"
+// 	"github.com/cosmos/cosmos-sdk/codec"
+// 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	"github.com/CosmosContracts/juno/v27/x/mint/types"
-)
+// 	"github.com/CosmosContracts/juno/v27/x/mint/types"
+// )
 
-const (
-	ModuleName = "mint"
-)
+// const (
+// 	ModuleName = "mint"
+// )
 
-var ParamsKey = []byte{0x01}
+// var ParamsKey = []byte{0x01}
 
-// Migrate migrates the x/mint module state from the consensus version 2 to
-// version 3. Specifically, it takes the parameters that are currently stored
-// and managed by the x/params modules and stores them directly into the x/mint
-// module state.
-func Migrate(
-	_ sdk.Context,
-	store sdk.KVStore,
-	cdc codec.BinaryCodec,
-	bondDenom string,
-) error {
-	// https://juno-api.reece.sh/cosmos/mint/v1beta1/params
-	currParams := types.Params{
-		MintDenom:     bondDenom,
-		BlocksPerYear: 5048093,
-	}
+// // Migrate migrates the x/mint module state from the consensus version 2 to
+// // version 3. Specifically, it takes the parameters that are currently stored
+// // and managed by the x/params modules and stores them directly into the x/mint
+// // module state.
+// func Migrate(
+// 	_ sdk.Context,
+// 	store sdk.KVStore,
+// 	cdc codec.BinaryCodec,
+// 	bondDenom string,
+// ) error {
+// 	// https://juno-api.reece.sh/cosmos/mint/v1beta1/params
+// 	currParams := types.Params{
+// 		MintDenom:     bondDenom,
+// 		BlocksPerYear: 5048093,
+// 	}
 
-	fmt.Printf("migrating %s params: %+v\n", ModuleName, currParams)
+// 	fmt.Printf("migrating %s params: %+v\n", ModuleName, currParams)
 
-	if err := currParams.Validate(); err != nil {
-		return err
-	}
+// 	if err := currParams.Validate(); err != nil {
+// 		return err
+// 	}
 
-	bz := cdc.MustMarshal(&currParams)
-	store.Set(ParamsKey, bz)
+// 	bz := cdc.MustMarshal(&currParams)
+// 	store.Set(ParamsKey, bz)
 
-	return nil
-}
+// 	return nil
+// }
